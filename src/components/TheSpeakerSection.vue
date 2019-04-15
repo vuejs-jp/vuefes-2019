1a09222f9c795088177eee92dca761e0f9686b20
INOUE Takuya <inouetakuya5@gmail.com>
2019-04-15 18:11:04 +0900

    TheSpeakerSection の 769px 以上の場合についてレイアウトを記述した

--- src/components/TheSpeakerSection.vue
+++ src/components/TheSpeakerSection.vue
@@ -1,54 +1,60 @@
 <template>
   <section class="the-speaker-section">
-    <h2 class="heading">
-      SPEAKERS
-    </h2>
+    <div class="section-container">
+      <h2 class="heading">
+        SPEAKERS
+      </h2>
 
-    <img
-      class="avatar"
-      :srcset="`${avatar}, ${avatar2x} 2x`"
-      :src="avatar2x"
-    />
+      <div class="speaker-container">
+        <img
+          class="avatar"
+          :srcset="`${avatar}, ${avatar2x} 2x`"
+          :src="avatar2x"
+        />
 
-    <div class="title">
-      Vue.js クリエーター
-    </div>
+        <div class="speaker-content">
+          <div class="title">
+            Vue.js クリエーター
+          </div>
 
-    <h3 class="name">
-      Evan you
-    </h3>
+          <h3 class="name">
+            Evan you
+          </h3>
 
-    <div class="social">
-      <a
-        class="twitter"
-        href="https://twitter.com/youyuxi"
-        target="_blank"
-        rel="noopener"
-      >
-        <img src="~/assets/images/icon-twitter.svg" />
-      </a>
-      <a
-        class="github"
-        href="https://github.com/yyx990803"
-        target="_blank"
-        rel="noopener"
-      >
-        <img src="~/assets/images/icon-github.svg" />
-      </a>
-    </div>
+          <div class="social">
+            <a
+              class="twitter"
+              href="https://twitter.com/youyuxi"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="~/assets/images/icon-twitter.svg" />
+            </a>
+            <a
+              class="github"
+              href="https://github.com/yyx990803"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="~/assets/images/icon-github.svg" />
+            </a>
+          </div>
 
-    <div class="description">
-      <p class="readable">
-        Evan は開発者、デザイナー、そしてクリエイティブコーダーです。彼は、リアクティブなコンポーネントでモダンな Web インターフェイスを構築するための JavaScript フレームワーク、Vue.js の作者です。
-      </p>
+          <div class="description">
+            <p>
+              Evan は開発者、デザイナー、そしてクリエイティブコーダーです。彼は、リアクティブなコンポーネントでモダンな Web インターフェイスを構築するための JavaScript フレームワーク、Vue.js の作者です。
+            </p>
 
-      <p class="readable">
-        かつて、GitHub で最もスターを集めたフルスタック JavaScript フレームワークであった Meteor の開発グループでも働いていました。Google Creative Lab で、さまざまな Google プロダクト向けの実験的な UI プロトタイプに、2年間取り組んでいた経験もあります。'
-      </p>
-    </div>
+            <p>
+              かつて、GitHub で最もスターを集めたフルスタック JavaScript フレームワークであった Meteor の開発グループでも働いていました。Google Creative Lab で、さまざまな Google プロダクト向けの実験的な UI プロトタイプに、2年間取り組んでいた経験もあります。
+            </p>
+          </div>
+        </div>
+      </div>
 
-    <div class="more">
-      他スピーカーは決定次第、更新予定です。
+      <div class="more">
+        他スピーカーは決定次第、更新予定です。
+      </div>
     </div>
   </section>
 </template>
@@ -87,6 +93,7 @@ export default class TheSpeakersSection extends Vue {
 }
 
 .name {
+  margin-top: 0;
   margin-bottom: 4px;
   font-size: 5.21vw;
 }
@@ -100,13 +107,38 @@ export default class TheSpeakersSection extends Vue {
   margin-left: 12px;
 }
 
-.description {
-  .readable {
-    font-size: 3.5vw;
-  }
+.description p {
+  font-size: 3.5vw;
 }
 
 .more {
   font-size: 3.5vw;
+}
+
+@media screen and (min-width: $layout-breakpoint--is-small-up) {
+  .the-speaker-section {
+    padding-left: 70px;
+    padding-right: 70px;
+  }
+
+  .section-container {
+    margin: 0 auto;
+    width: calc(100% - 140px);
+    min-width: 70%; // TODO: これで合っているか要確認
+  }
+
+  .speaker-container {
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .avatar {
+    width: calc((2 / 7) * 100%);
+    margin-right: 40px;
+  }
+
+  .speaker-content {
+    width: calc((5 / 7) * 100%);
+  }
 }
 </style>
