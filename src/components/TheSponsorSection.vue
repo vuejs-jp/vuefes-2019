6d38e52270e2d1e3ca40225b1918349c3a9eecac
ryamakuchi <r.yamakuchi@gmail.com>
2019-04-23 00:34:49 +0900

    BaseSection を利用する

--- src/components/TheSponsorSection.vue
+++ src/components/TheSponsorSection.vue
@@ -1,8 +1,32 @@
 <template>
-  <section class="the-sponsor-section">
-    SPONSORS
-  </section>
+  <BaseSection class="the-sponsor-section">
+    <template slot="heading">
+      SPONSORS
+    </template>
+
+    <div class="description">
+      <p>
+        Vue.jsに関わる人々が集まるVue Fes Japan 2019をよりよいイベントにするため、スポンサーを募集します。募集開始は、2019年4月下旬を予定しています。
+      </p>
+
+      <p>
+        最新情報は、Vue Fes JapanのTwitterでご確認ください。
+      </p>
+    </div>
+  </BaseSection>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import BaseSection from '~/components/BaseSection.vue'
+
+@Component({
+  components: {
+    BaseSection
+  }
+})
+export default class TheSponsorsSection extends Vue {}
+</script>
 
 <style lang="scss" scoped>
 .the-sponsor-section {
