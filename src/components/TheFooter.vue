1c76511ee4ab150182282e4a3f02e5f19fe63428
yinm <yinm.cminor9@gmail.com>
2019-04-23 21:05:11 +0900

    リストを段組みレイアウトにする

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,88 +1,92 @@
 <template>
   <footer class="the-footer">
     <div class="footer-container">
-      <img src="~/assets/images/logo-vuefes.svg" alt="VueFes" />
-
-      <div class="menu-list">
-        <h3>メニュー</h3>
-        <nuxt-link to="/code-of-conduct/">
-          行動規範
-        </nuxt-link>
-        <nuxt-link to="/privacy/">
-          プライバシーポリシー
-        </nuxt-link>
-        <nuxt-link to="/contact/">
-          お問い合わせ
-        </nuxt-link>
-      </div>
-
-      <div class="social-list">
-        <h3>ソーシャル</h3>
-        <a
-          href="https://twitter.com/vuefes"
-          target="_blank"
-          rel="noopener"
-        >
-          <img src="~/assets/images/white-icon-twitter.svg" />
-          Twitter
-        </a>
-        <!-- TODO: URLを確認する -->
-        <a
-          href=""
-          target="_blank"
-          rel="noopener"
-        >
-          <img src="~/assets/images/white-icon-youtube.svg" />
-          YouTube
-        </a>
-        <!-- TODO: URLを確認する -->
-        <a
-          href=""
-          target="_blank"
-          rel="noopener"
-        >
-          <img src="~/assets/images/white-icon-github.svg" />
-          GitHub
-        </a>
-      </div>
-
-      <div class="link-list">
-        <h3>各種リンク</h3>
-        <a
-          href="https://jp.vuejs.org/"
-          target="_blank"
-          rel="noopener"
-        >
-          Vue.js
-        </a>
-        <a
-          href="https://github.com/vuejs-jp/home"
-          target="_blank"
-          rel="noopener"
-        >
-          Vue.js 日本ユーザーグループ
-        </a>
-        <a
-          href="https://vuefes.jp/2018/"
-          target="_blank"
-          rel="noopener"
-        >
-          Vue Fes Japan 2018
-        </a>
-      </div>
-
-      <div class="social-wrapper">
-        <div class="social-button" @click="openTwitterForm">
-          <img src="~/assets/images/logo-twitter.svg" alt="Twitter" />
+      <div class="content-wrapper">
+        <div class="logo">
+          <img src="~/assets/images/logo-vuefes.svg" alt="VueFes" />
         </div>
-        <div class="social-button" @click="openFacebookForm">
-          <img src="~/assets/images/logo-facebook.svg" alt="Facebook" />
+
+        <div class="menu-list">
+          <h3>メニュー</h3>
+          <nuxt-link to="/code-of-conduct/">
+            行動規範
+          </nuxt-link>
+          <nuxt-link to="/privacy/">
+            プライバシーポリシー
+          </nuxt-link>
+          <nuxt-link to="/contact/">
+            お問い合わせ
+          </nuxt-link>
         </div>
-        <div class="social-button" @click="openHatenaBookmarkForm">
-          <img
-            src="~/assets/images/logo-hatena-bookmark.svg"
-            alt="はてなブックマーク"
-          />
+
+        <div class="social-list">
+          <h3>ソーシャル</h3>
+          <a
+            href="https://twitter.com/vuefes"
+            target="_blank"
+            rel="noopener"
+          >
+            <img src="~/assets/images/white-icon-twitter.svg" />
+            Twitter
+          </a>
+          <!-- TODO: URLを確認する -->
+          <a
+            href=""
+            target="_blank"
+            rel="noopener"
+          >
+            <img src="~/assets/images/white-icon-youtube.svg" />
+            YouTube
+          </a>
+          <!-- TODO: URLを確認する -->
+          <a
+            href=""
+            target="_blank"
+            rel="noopener"
+          >
+            <img src="~/assets/images/white-icon-github.svg" />
+            GitHub
+          </a>
+        </div>
+
+        <div class="link-list">
+          <h3>各種リンク</h3>
+          <a
+            href="https://jp.vuejs.org/"
+            target="_blank"
+            rel="noopener"
+          >
+            Vue.js
+          </a>
+          <a
+            href="https://github.com/vuejs-jp/home"
+            target="_blank"
+            rel="noopener"
+          >
+            Vue.js 日本ユーザーグループ
+          </a>
+          <a
+            href="https://vuefes.jp/2018/"
+            target="_blank"
+            rel="noopener"
+          >
+            Vue Fes Japan 2018
+          </a>
+        </div>
+
+        <div class="social-wrapper">
+          <div class="social-button" @click="openTwitterForm">
+            <img src="~/assets/images/logo-twitter.svg" alt="Twitter" />
+          </div>
+          <div class="social-button" @click="openFacebookForm">
+            <img src="~/assets/images/logo-facebook.svg" alt="Facebook" />
+          </div>
+          <div class="social-button" @click="openHatenaBookmarkForm">
+            <img
+              src="~/assets/images/logo-hatena-bookmark.svg"
+              alt="はてなブックマーク"
+            />
+          </div>
         </div>
       </div>
 
@@ -158,11 +162,26 @@
     margin-left: 13.75%;
 
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
-      margin-left: 22px;
+      margin-left: 15px;
     }
   }
 
   img {
+    display: block;
+  }
+}
+
+.logo img {
+  display: block;
+  max-width: 220px;
+}
+
+.content-wrapper {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 40px;
+
+  a {
     display: block;
   }
 }
