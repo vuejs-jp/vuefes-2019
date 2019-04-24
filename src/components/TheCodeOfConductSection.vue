98620cacc7d7d900b522f2de09eeafe45af7e82a
yinm <yinm.cminor9@gmail.com>
2019-04-24 18:03:30 +0900

    行動規範のページと、コンポーネントを追加する

---
+++ src/components/TheCodeOfConductSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseSection class="the-code-of-conduct-section">
+    <template slot="heading">
+      行動規範
+    </template>
+  </BaseSection>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import BaseSection from '~/components/BaseSection.vue'
+
+@Component({
+  components: {
+    BaseSection
+  }
+})
+export default class TheCodeOfConductSection extends Vue {}
+</script>
---
+++ src/pages/code-of-conduct.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="code-of-conduct">
+    <TheCodeOfConductSection />
+    <TheFooter />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import TheCodeOfConductSection from '~/components/TheCodeOfConductSection.vue'
+import TheFooter from '~/components/TheFooter.vue'
+
+@Component({
+  components: {
+    TheCodeOfConductSection,
+    TheFooter
+  }
+})
+export default class CodeOfConduct extends Vue {}
+</script>
