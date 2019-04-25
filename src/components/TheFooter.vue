e7cbe915d0800589e85538cd568076160f6b072a
yinm <yinm.cminor9@gmail.com>
2019-04-25 20:29:40 +0900

    小さいディスプレイの時のスタイルを調整

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,78 +6,6 @@
           <img src="~/assets/images/logo-vuefes.svg" alt="VueFes" />
         </div>
 
-        <div class="menu-list">
-          <h3 class="list-title">
-            メニュー
-          </h3>
-          <nuxt-link to="/code-of-conduct/">
-            行動規範
-          </nuxt-link>
-          <nuxt-link to="/privacy/">
-            プライバシーポリシー
-          </nuxt-link>
-          <nuxt-link to="/contact/">
-            お問い合わせ
-          </nuxt-link>
-        </div>
-
-        <div class="social-list">
-          <h3 class="list-title">
-            ソーシャル
-          </h3>
-          <a
-            href="https://twitter.com/vuefes"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src="~/assets/images/white-icon-twitter.svg" />
-            Twitter
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UC6KPwA1kZJtQYdlh8_2hxCA"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src="~/assets/images/white-icon-youtube.svg" />
-            YouTube
-          </a>
-          <a
-            href="https://github.com/vuejs-jp/home"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src="~/assets/images/white-icon-github.svg" />
-            GitHub
-          </a>
-        </div>
-
-        <div class="link-list">
-          <h3 class="list-title">
-            各種リンク
-          </h3>
-          <a
-            href="https://jp.vuejs.org/"
-            target="_blank"
-            rel="noopener"
-          >
-            Vue.js
-          </a>
-          <a
-            href="https://github.com/vuejs-jp/home"
-            target="_blank"
-            rel="noopener"
-          >
-            Vue.js 日本ユーザーグループ
-          </a>
-          <a
-            href="https://vuefes.jp/2018/"
-            target="_blank"
-            rel="noopener"
-          >
-            Vue Fes Japan 2018
-          </a>
-        </div>
-
         <div class="social-wrapper">
           <div class="social-button" @click="openTwitterForm">
             <img src="~/assets/images/logo-twitter.svg" alt="Twitter" />
@@ -90,6 +18,80 @@
               src="~/assets/images/logo-hatena-bookmark.svg"
               alt="はてなブックマーク"
             />
+          </div>
+        </div>
+
+        <div class="list-wrapper">
+          <div class="menu-list">
+            <h3 class="list-title">
+              メニュー
+            </h3>
+            <nuxt-link to="/code-of-conduct/">
+              行動規範
+            </nuxt-link>
+            <nuxt-link to="/privacy/">
+              プライバシーポリシー
+            </nuxt-link>
+            <nuxt-link to="/contact/">
+              お問い合わせ
+            </nuxt-link>
+          </div>
+
+          <div class="social-list">
+            <h3 class="list-title">
+              ソーシャル
+            </h3>
+            <a
+              href="https://twitter.com/vuefes"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="~/assets/images/white-icon-twitter.svg" />
+              Twitter
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UC6KPwA1kZJtQYdlh8_2hxCA"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="~/assets/images/white-icon-youtube.svg" />
+              YouTube
+            </a>
+            <a
+              href="https://github.com/vuejs-jp/home"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="~/assets/images/white-icon-github.svg" />
+              GitHub
+            </a>
+          </div>
+
+          <div class="link-list">
+            <h3 class="list-title">
+              各種リンク
+            </h3>
+            <a
+              href="https://jp.vuejs.org/"
+              target="_blank"
+              rel="noopener"
+            >
+              Vue.js
+            </a>
+            <a
+              href="https://github.com/vuejs-jp/home"
+              target="_blank"
+              rel="noopener"
+            >
+              Vue.js 日本ユーザーグループ
+            </a>
+            <a
+              href="https://vuefes.jp/2018/"
+              target="_blank"
+              rel="noopener"
+            >
+              Vue Fes Japan 2018
+            </a>
           </div>
         </div>
       </div>
@@ -116,7 +118,7 @@
 
 <style lang="scss" scoped>
 .the-footer {
-  padding: 70px;
+  padding: 60px 7.8%;
   background-color: $primary-color;
   color: $primary-text-color--invert;
 
@@ -141,9 +143,26 @@
   }
 }
 
+.content-wrapper {
+  a {
+    display: block;
+  }
+}
+
+.logo {
+  float: left;
+  width: 50%;
+
+  img {
+    width: 97%;
+  }
+}
+
 .social-wrapper {
+  float: left;
+  width: 50%;
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   margin-bottom: 30px;
 
   @media screen and (min-width: $layout-breakpoint--is-small-up) {
@@ -153,7 +172,7 @@
 }
 
 .social-button {
-  width: 13.75%;
+  width: 20%;
   max-width: 57px;
 
   @media screen and (min-width: $layout-breakpoint--is-small-up) {
@@ -163,7 +182,7 @@
   }
 
   & + & {
-    margin-left: 13.75%;
+    margin-left: 8.8%;
 
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       margin-left: 15px;
@@ -175,41 +194,48 @@
   }
 }
 
-.logo img {
-  display: block;
-  max-width: 220px;
-}
-
-.content-wrapper {
+.list-wrapper {
+  clear: both;
   display: flex;
-  justify-content: space-between;
-  margin-bottom: 40px;
-
-  a {
-    display: block;
-  }
+  flex-wrap: wrap;
 }
 
 .menu-list,
 .social-list,
 .link-list {
+  width: 50%;
   font-size: 14px;
   line-height: 2;
 
   .list-title {
     margin-top: 0;
-    font-size: 18px;
+    font-size: 6vw;
     font-weight: bold;
   }
 
   a {
-    margin-bottom: 10px;
+    font-size: 4vw;
+    margin-bottom: 15%;
+    line-height: 1.2;
     text-decoration: none;
   }
 }
 
 .licence p {
-  font-size: 12px;
+  font-size: 2.5vw;
   font-weight: 300;
+}
+
+@media screen and (min-width: $layout-breakpoint--is-small-up) {
+  .content-wrapper {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 40px;
+  }
+
+  .logo img {
+    display: block;
+    max-width: 220px;
+  }
 }
 </style>
