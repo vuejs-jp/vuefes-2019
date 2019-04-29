80eaf9f42cb6034c37596441de993e4c7e7a100b
mtmtkzm <hello@mtmtkzm.com>
2019-04-30 02:42:32 +0900

    teammates.ts を コンポーネント内に閉じ込める

--- src/components/TheStaffListSection.vue
+++ src/components/TheStaffListSection.vue
@@ -33,15 +33,149 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import BaseSection from '~/components/BaseSection.vue'
-import teammates from '~/assets/data/teammates.ts'
-
+interface Teammate {
+  name: string
+  link: string
+  avatar: string
+}
 @Component({
   components: {
     BaseSection
   }
 })
 export default class TheCallForPresentersSection extends Vue {
-  private teammates = teammates
+  teammates: Teammate[] = [
+    {
+      name: 'kazu_pon',
+      link: 'https://twitter.com/kazu_pon',
+      avatar: 'kazu_pon.png'
+    },
+    {
+      name: '448jp',
+      link: 'https://twitter.com/448jp',
+      avatar: '448jp.jpg'
+    },
+    {
+      name: 'kawakami0717',
+      link: 'https://twitter.com/kawakami0717',
+      avatar: 'kawakami0717.png'
+    },
+    {
+      name: 'kazuyukimiyake',
+      link: 'https://twitter.com/kazuyukimiyake',
+      avatar: 'kazuyukimiyake.jpg'
+    },
+    {
+      name: 'inouetakuya',
+      link: 'https://twitter.com/inouetakuya',
+      avatar: 'inouetakuya.jpg'
+    },
+    {
+      name: 'c5meru',
+      link: 'https://twitter.com/c5meru',
+      avatar: 'c5meru.jpg'
+    },
+    {
+      name: 'hashedrock',
+      link: 'https://twitter.com/hashedrock',
+      avatar: 'hashedrock.jpg'
+    },
+    {
+      name: 'shika358',
+      link: 'https://twitter.com/shika358',
+      avatar: 'shika358.jpg'
+    },
+    {
+      name: 'potato4d',
+      link: 'https://twitter.com/potato4d',
+      avatar: 'potato4d.jpg'
+    },
+    {
+      name: 'positiveflat',
+      link: 'https://twitter.com/positiveflat',
+      avatar: 'positiveflat.png'
+    },
+    {
+      name: 'andoshin11',
+      link: 'https://twitter.com/andoshin11',
+      avatar: 'andoshin11.jpg'
+    },
+    {
+      name: 'yakiniku040220',
+      link: 'https://twitter.com/yakiniku040220',
+      avatar: 'yakiniku040220.jpg'
+    },
+    {
+      name: 'tsuchikazu',
+      link: 'https://twitter.com/tsuchikazu',
+      avatar: 'tsuchikazu.png'
+    },
+    {
+      name: '__syumai',
+      link: 'https://twitter.com/__syumai',
+      avatar: '__syumai.png'
+    },
+    {
+      name: '_yinm_',
+      link: 'https://twitter.com/_yinm_',
+      avatar: 'yinm.png'
+    },
+    {
+      name: 'ryamakuchi',
+      link: 'https://twitter.com/ryamakuchi',
+      avatar: 'ryamakuchi.png'
+    },
+    {
+      name: 'treby006',
+      link: 'https://twitter.com/treby006',
+      avatar: 'treby006.png'
+    },
+    {
+      name: 'massie',
+      link: 'https://twitter.com/massie',
+      avatar: 'massie.png'
+    },
+    {
+      name: 'nicedchy',
+      link: 'https://twitter.com/nicedchy',
+      avatar: 'nicedchy.png'
+    },
+    {
+      name: 'lenomick',
+      link: 'https://twitter.com/lenomick',
+      avatar: 'lenomick.png'
+    },
+    {
+      name: 'naokie',
+      link: 'https://twitter.com/naokie',
+      avatar: 'naokie.png'
+    },
+    {
+      name: 'takumisatojpn',
+      link: 'https://twitter.com/takumisatojpn',
+      avatar: 'takumisatojpn.png'
+    },
+    {
+      name: 'yotaszk',
+      link: 'https://twitter.com/yotaszk',
+      avatar: 'yotaszk.png'
+    },
+    {
+      name: 'mtmtkzm',
+      link: 'https://twitter.com/mtmtkzm',
+      avatar: 'mtmtkzm.jpg'
+    },
+    {
+      name: 'hisako135',
+      link: 'https://note.mu/hisahisa135',
+      avatar: 'hisako135.jpg'
+    },
+    {
+      name: 'aytdm251',
+      link: 'https://twitter.com/aytdm251',
+      avatar: 'aytdm251.jpg'
+    }
+  ]
 }
 </script>
 
