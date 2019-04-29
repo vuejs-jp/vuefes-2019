19d84fd80a0f612746cdfcf45527c29eb201aa1f
INOUE Takuya <inouetakuya5@gmail.com>
2019-04-29 23:04:44 +0900

    fix: [Vue warn]: Unknown custom element: <fa>

--- test/unit/specs/pages/code-of-conduct.spec.ts
+++ test/unit/specs/pages/code-of-conduct.spec.ts
@@ -1,11 +1,13 @@
 import { mount, RouterLinkStub } from '@vue/test-utils'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import CodeOfConductPage from '~/pages/code-of-conduct.vue'
 
 describe('CodeOfConductPage', () => {
   test('レンダリングできる', () => {
     const wrapper = mount(CodeOfConductPage, {
       stubs: {
-        NuxtLink: RouterLinkStub
+        NuxtLink: RouterLinkStub,
+        Fa: FontAwesomeIcon
       }
     })
     expect(wrapper.find('.code-of-conduct').isVisible()).toBeTruthy()
