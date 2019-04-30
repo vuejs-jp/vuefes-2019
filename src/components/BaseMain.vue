5fd690f40210041013911fa032972e1e02389de7
ryamakuchi <r.yamakuchi@gmail.com>
2019-04-30 13:48:47 +0900

    BaseMain コンポーネントを作成

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,75 @@
+<template>
+  <main class="base-main">
+    <div class="main-container">
+      <h1 v-if="$slots.heading" class="heading">
+        <slot name="heading" />
+      </h1>
+
+      <slot />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class BaseMain extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.base-main {
+  padding: 110px 7.8% 100px;
+  background-image: url('~assets/images/texture.png');
+}
+
+.heading {
+  margin-bottom: 34px;
+  font-size: 10.41vw;
+  font-weight: bold;
+  line-height: 1;
+}
+
+section {
+  margin-bottom: 65px;
+}
+
+h2 {
+  margin-bottom: 25px;
+  font-size: 5.2vw;
+  font-weight: bold;
+  line-height: 1;
+}
+
+p {
+  font-size: 4vw;
+}
+
+@media screen and (min-width: $layout-breakpoint--is-small-up) {
+  .base-main {
+    padding: 130px 70px 80px;
+  }
+
+  .main-container {
+    margin: 0 auto;
+  }
+
+  .heading {
+    font-size: 80px;
+  }
+
+  h2 {
+    font-size: 42px;
+  }
+
+  p {
+    font-size: 18px;
+  }
+}
+
+@media screen and (min-width: $layout-breakpoint--is-medium-up) {
+  .main-container {
+    max-width: $page-container-max-width;
+  }
+}
+</style>
---
+++ src/pages/privacy.vue
@@ -0,0 +1,48 @@
+<template>
+  <BaseMain class="privacy">
+    <template slot="heading">
+      プライバシーポリシー
+    </template>
+  </BaseMain>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import BaseMain from '~/components/BaseMain.vue'
+import BaseButton from '~/components/BaseButton.vue'
+
+@Component({
+  components: {
+    BaseMain,
+    BaseButton
+  },
+  asyncData({ route }) {
+    return {
+      path: route.path
+    }
+  }
+})
+export default class CodeOfConductPage extends Vue {
+  path!: string
+
+  private head() {
+    const url = `https://vuefes.jp/2019${this.path}`
+    const title = 'プライバシーポリシー - Vue Fes Japan 2019'
+    const description = 'Vue Fes Japan 2019 のプライバシーポリシー情報です。'
+    return {
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'og:url', name: 'og:url', content: url },
+        { hid: 'og:title', name: 'og:title', content: title },
+        { hid: 'og:description', name: 'og:description', content: description },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: description
+        }
+      ]
+    }
+  }
+}
+</script>
