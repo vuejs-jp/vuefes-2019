3bea0f49ea6b5997285fe2bea3d71e29840b9edc
ryamakuchi <r.yamakuchi@gmail.com>
2019-05-05 17:57:24 +0900

    contact ページに OGP 情報などを追加

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="contact">
+  <div class="contact-page">
     <ContactForm />
   </div>
 </template>
@@ -7,12 +7,39 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import ContactForm from '~/components/ContactForm.vue'
-import TheFooter from '~/components/TheFooter.vue'
+
 @Component({
   components: {
-    ContactForm,
-    TheFooter
+    ContactForm
+  },
+  asyncData({ route }) {
+    return {
+      path: route.path
+    }
   }
 })
-export default class ContactPage extends Vue {}
+export default class ContactPage extends Vue {
+  path!: string
+
+  private head() {
+    const url = `https://vuefes.jp/2019${this.path}`
+    const title = 'お問い合わせ - Vue Fes Japan 2019'
+    const description = 'Vue Fes Japan 2019 についてのお問い合わせフォームです。'
+    return {
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'og:url', name: 'og:url', content: url },
+        { hid: 'og:title', name: 'og:title', content: title },
+        { hid: 'og:description', name: 'og:description', content: description },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: description
+        }
+      ]
+    }
+  }
+}
 </script>
