a759a3eba2505ec6ef4a6c49ba0e78cb0f022aed
INOUE Takuya <inouetakuya5@gmail.com>
2019-05-06 16:28:11 +0900

    実現したいことを todo で書き出した

--- test/unit/specs/components/TheStaffListSection.spec.ts
+++ test/unit/specs/components/TheStaffListSection.spec.ts
@@ -6,4 +6,9 @@ describe('TheStaffListSection', () => {
     const wrapper = mount(TheStaffListSection)
     expect(wrapper.find('.the-staff-list-section').isVisible()).toBeTruthy()
   })
+
+  describe('compareStaffs', () => {
+    test.todo('英大文字・小文字を区別しない')
+    test.todo('英字 / 数字 / 記号の順でソートする')
+  })
 })
