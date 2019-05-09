70231b19be94f7afc2913082a2bc54e0a10e060e
INOUE Takuya <inouetakuya5@gmail.com>
2019-05-09 21:58:23 +0900

    og:* と twitter 用のメタ情報を設定した

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import NuxtConfiguration from '@nuxt/config'
 import StylelintPlugin from 'stylelint-webpack-plugin'
 import hooks from './src/hooks'
 
+const defaultUrl = 'https://vuefes.jp/2019/'
+const defaultTitle = 'Vue Fes Japan 2019'
+const defaultDescription =
+  '2019年10月12日（土）に開催される日本最大級の Vue.js カンファレンス。国内外の著名スピーカーによるセッションの他、ユーザー同士が気軽に話し合える場も設ける予定です。ぜひ、一緒に Vue.js を楽しみ、盛り上げていきましょう！'
 const defaultOgImageUrl: string = 'https://vuefes.jp/2019/opengraph.png'
 
 const config: NuxtConfiguration = {
@@ -12,19 +16,42 @@ const config: NuxtConfiguration = {
   },
   hooks: hooks(this),
   head: {
-    title: 'vuefes-2019',
+    title: defaultTitle,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'og:locale', content: 'ja_JP' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:site_name', content: 'Vue Fes Japan 2019' },
+      { name: 'twitter:creator', content: '@vuefes' },
+      { hid: 'description', name: 'description', content: defaultDescription },
+      { hid: 'og:url', name: 'og:url', content: defaultUrl },
+      { hid: 'og:title', name: 'og:title', content: defaultTitle },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Vue Fes Japan 2019'
+        hid: 'og:description',
+        name: 'og:description',
+        content: defaultDescription
       },
       { hid: 'og:image', name: 'og:image', content: defaultOgImageUrl },
       {
         hid: 'og:image:secure_url',
         name: 'og:image:secure_url',
+        content: defaultOgImageUrl
+      },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      { hid: 'twitter:title', name: 'twitter:title', content: defaultTitle },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: defaultDescription
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
         content: defaultOgImageUrl
       }
     ],
