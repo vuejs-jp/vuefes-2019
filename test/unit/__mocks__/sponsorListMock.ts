83be33994100b6e9021c7bdda55e2e674f3a8089
INOUE Takuya <inouetakuya5@gmail.com>
2019-05-31 19:49:21 +0900

    new file: test/unit/__mocks__/sponsorListMock.ts

---
+++ test/unit/__mocks__/sponsorListMock.ts
@@ -0,0 +1,243 @@
+import { Entry } from 'contentful/index'
+
+const sponsorList: Entry<any>[] = [
+  {
+    sys: {
+      space: {
+        sys: {
+          type: 'Link',
+          linkType: 'Space',
+          id: 'makqrw5hfm9d'
+        }
+      },
+      id: '4MobjBuuQiZZSoG817NTjH',
+      type: 'Entry',
+      createdAt: '2019-05-19T01:42:49.169Z',
+      updatedAt: '2019-05-19T01:43:22.698Z',
+      environment: {
+        sys: {
+          id: 'master',
+          type: 'Link',
+          linkType: 'Environment'
+        }
+      },
+      revision: 2,
+      contentType: {
+        sys: {
+          type: 'Link',
+          linkType: 'ContentType',
+          id: 'sponsor'
+        }
+      },
+      locale: 'en-US'
+    },
+    fields: {
+      type: 'silver',
+      name: '株式会社ピースオブケイク',
+      url: 'https://www.pieceofcake.co.jp/',
+      banner: {
+        sys: {
+          space: {
+            sys: {
+              type: 'Link',
+              linkType: 'Space',
+              id: 'makqrw5hfm9d'
+            }
+          },
+          id: '5oE0xfAFUQDBvjcgxv4s7m',
+          type: 'Asset',
+          createdAt: '2019-05-19T01:41:20.657Z',
+          updatedAt: '2019-05-19T01:41:20.657Z',
+          environment: {
+            sys: {
+              id: 'master',
+              type: 'Link',
+              linkType: 'Environment'
+            }
+          },
+          revision: 1,
+          locale: 'en-US'
+        },
+        fields: {
+          title: 'note',
+          file: {
+            url:
+              '//images.ctfassets.net/makqrw5hfm9d/5oE0xfAFUQDBvjcgxv4s7m/a14d86104f7cadc1ac4d948cc7ec205d/note.png',
+            details: {
+              size: 1943,
+              image: {
+                width: 225,
+                height: 225
+              }
+            },
+            fileName: 'note.png',
+            contentType: 'image/png'
+          }
+        }
+      },
+      description:
+        '株式会社ピースオブケイクは、デジタルコンテンツの未来を切り拓くために設立されました。\n\nクリエイターのみなさんが、気持ちよくものづくりをして、受け手のみなさんに気持ちよく届ける。\nコンテンツとテクノロジーとデザインを融合させた、新しいプラットフォームをつくります。',
+      publishedAt: '2019-05-21T10:00+09:00'
+    }
+  },
+  {
+    sys: {
+      space: {
+        sys: {
+          type: 'Link',
+          linkType: 'Space',
+          id: 'makqrw5hfm9d'
+        }
+      },
+      id: '267g2hobCdNcaDAOsbB6go',
+      type: 'Entry',
+      createdAt: '2019-05-19T01:35:11.764Z',
+      updatedAt: '2019-05-19T01:35:11.764Z',
+      environment: {
+        sys: {
+          id: 'master',
+          type: 'Link',
+          linkType: 'Environment'
+        }
+      },
+      revision: 1,
+      contentType: {
+        sys: {
+          type: 'Link',
+          linkType: 'ContentType',
+          id: 'sponsor'
+        }
+      },
+      locale: 'en-US'
+    },
+    fields: {
+      type: 'gold',
+      name: '合同会社 esa',
+      url: 'https://team.esa.io/',
+      banner: {
+        sys: {
+          space: {
+            sys: {
+              type: 'Link',
+              linkType: 'Space',
+              id: 'makqrw5hfm9d'
+            }
+          },
+          id: '5HGLyXJQPYKOYSk7aG0DhK',
+          type: 'Asset',
+          createdAt: '2019-05-19T01:34:18.682Z',
+          updatedAt: '2019-05-19T01:34:18.682Z',
+          environment: {
+            sys: {
+              id: 'master',
+              type: 'Link',
+              linkType: 'Environment'
+            }
+          },
+          revision: 1,
+          locale: 'en-US'
+        },
+        fields: {
+          title: 'esa',
+          file: {
+            url:
+              '//images.ctfassets.net/makqrw5hfm9d/5HGLyXJQPYKOYSk7aG0DhK/e666a9f255850fe9cb55dba0cdcb2657/esa.png',
+            details: {
+              size: 3304,
+              image: {
+                width: 225,
+                height: 225
+              }
+            },
+            fileName: 'esa.png',
+            contentType: 'image/png'
+          }
+        }
+      },
+      description:
+        'esaは情報を育てるという視点で作られた自律的なチームのための情報共有サービスです。',
+      publishedAt: '2019-05-20T10:00+09:00'
+    }
+  },
+  {
+    sys: {
+      space: {
+        sys: {
+          type: 'Link',
+          linkType: 'Space',
+          id: 'makqrw5hfm9d'
+        }
+      },
+      id: '7roYcJn8fueWVlkAPKpCY3',
+      type: 'Entry',
+      createdAt: '2019-05-19T01:31:53.021Z',
+      updatedAt: '2019-05-19T01:31:53.021Z',
+      environment: {
+        sys: {
+          id: 'master',
+          type: 'Link',
+          linkType: 'Environment'
+        }
+      },
+      revision: 1,
+      contentType: {
+        sys: {
+          type: 'Link',
+          linkType: 'ContentType',
+          id: 'sponsor'
+        }
+      },
+      locale: 'en-US'
+    },
+    fields: {
+      type: 'gold',
+      name: 'アン・コンサルティング株式会社',
+      url: 'https://www.anconsulting.jp/',
+      banner: {
+        sys: {
+          space: {
+            sys: {
+              type: 'Link',
+              linkType: 'Space',
+              id: 'makqrw5hfm9d'
+            }
+          },
+          id: '7w51pGQ6OHdOZvp15i5R4h',
+          type: 'Asset',
+          createdAt: '2019-05-19T01:30:16.615Z',
+          updatedAt: '2019-05-19T01:30:16.615Z',
+          environment: {
+            sys: {
+              id: 'master',
+              type: 'Link',
+              linkType: 'Environment'
+            }
+          },
+          revision: 1,
+          locale: 'en-US'
+        },
+        fields: {
+          title: 'anconsulting',
+          file: {
+            url:
+              '//images.ctfassets.net/makqrw5hfm9d/7w51pGQ6OHdOZvp15i5R4h/67a7c9f1113ee6fe24441b883bc4fa6f/anconsulting.png',
+            details: {
+              size: 3713,
+              image: {
+                width: 225,
+                height: 225
+              }
+            },
+            fileName: 'anconsulting.png',
+            contentType: 'image/png'
+          }
+        }
+      },
+      description:
+        'アン・コンサルティングは、フリーランス IT エンジニア向け案件・求人・仕事「フリエン」を運営しています。\n\n金融業界、IT業界など各業界で経験を積んだ日本の IT 人材マーケットに精通したスペシャリスト集団です。',
+      publishedAt: '2019-05-19T10:40+09:00'
+    }
+  }
+]
+
+export default sponsorList
