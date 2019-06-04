63e2f84d415c838ebe32fe3c8d2c26dc9fc4e40d
Yusuke Iinuma <yinm@users.noreply.github.com>
2019-06-05 00:22:15 +0900

    fix: テスト実行時に [Vue warn]: Failed to resolve directive: lazy-container (#71)
    * Fix  [Vue warn]: Failed to resolve directive: lazy-container

--- test/unit/specs/components/TheSponsorSection.spec.ts
+++ test/unit/specs/components/TheSponsorSection.spec.ts
@@ -1,9 +1,14 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import VueLazyLoad from 'vue-lazyload'
 import TheSponsorSection from '~/components/TheSponsorSection.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(VueLazyLoad)
 
 describe('TheSponsorSection', () => {
   test('レンダリングできる', () => {
-    const wrapper = mount(TheSponsorSection)
+    const wrapper = mount(TheSponsorSection, { localVue })
     expect(wrapper.find('.the-sponsor-section').isVisible()).toBeTruthy()
   })
 })
