f658e663816dc55695c6bd2d309c2271d63bd856
INOUE Takuya <inouetakuya5@gmail.com>
2019-06-17 15:03:22 +0900

    NuxtLink がルートパス指定になっており、かつ Trailing Slash が入っていることのデグレーションテストを書いた

--- test/unit/specs/components/TheFooter.spec.ts
+++ test/unit/specs/components/TheFooter.spec.ts
@@ -3,13 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import TheFooter from '~/components/TheFooter.vue'
 
 describe('TheFooter', () => {
-  test('レンダリングできる', () => {
-    const wrapper = mount(TheFooter, {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(TheFooter, {
       stubs: {
         NuxtLink: RouterLinkStub,
         Fa: FontAwesomeIcon
       }
     })
+  })
+
+  test('レンダリングできる', () => {
     expect(wrapper.find('.the-footer').isVisible()).toBeTruthy()
+  })
+
+  test('NuxtLink がルートパス指定になっており、かつ Trailing Slash が入っている', () => {
+    for (const link of wrapper.findAll('.menu-list a').wrappers) {
+      expect(link.props().to).toEqual(expect.stringMatching(/^\/[\w-]+\/$/))
+    }
   })
 })
