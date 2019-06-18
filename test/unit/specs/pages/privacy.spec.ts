a59a26b526164bffab9653e7f7ef389de5a0303c
INOUE Takuya <inouetakuya5@gmail.com>
2019-06-18 11:19:12 +0900

    fix: トップページ以外でリロード後に nuxt-link のパスがズレる (#91)
    * fix: トップページ以外でリロード後に nuxt-link のパスがズレる
    
    * NuxtLink がルートパス指定になっており、かつ Trailing Slash が入っていることのデグレーションテストを書いた
    
    * 可読性を上げるために型アサーションを書いておく
    
    * 行動規範ページにも NuxtLink があったので Trailing Slash を入れた
    
    * BaseButton で to プロパティを使っている箇所にもデグレーションテストを書いた
    
    * 正規表現が甘かった箇所を修正した
    
    * fix: テストの正規表現がガバガバだった
    
    * テストのためだけに class="nuxt-link" を書かなくて良いようにした
    
    * プライバシーポリシーページには NuxtLink はひとつしかないので最小限の記述にした
    
    * fix: shallowMount だと wrapper.find(RouterLinkStub) で取得できなかった

--- test/unit/specs/pages/privacy.spec.ts
+++ test/unit/specs/pages/privacy.spec.ts
@@ -1,13 +1,23 @@
-import { shallowMount, RouterLinkStub } from '@vue/test-utils'
+import Vue from 'vue'
+import { mount, RouterLinkStub, Wrapper } from '@vue/test-utils'
 import PrivacyPage from '~/pages/privacy.vue'
 
 describe('PrivacyPage', () => {
-  test('レンダリングできる', () => {
-    const wrapper = shallowMount(PrivacyPage, {
+  let wrapper: Wrapper<Vue>
+
+  beforeEach(() => {
+    wrapper = mount(PrivacyPage, {
       stubs: {
         NuxtLink: RouterLinkStub
       }
     })
+  })
+
+  test('レンダリングできる', () => {
     expect(wrapper.find('.privacy-page').isVisible()).toBeTruthy()
+  })
+
+  test('NuxtLink がルートパス指定になっており、かつ Trailing Slash が入っている', () => {
+    expect(wrapper.find(RouterLinkStub).props().to).toBe('/')
   })
 })
