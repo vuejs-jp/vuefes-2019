b9e14ba4089cb8f041a9fc471226128bc97efb17
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-07 19:47:54 +0900

    キーノートを追加した

--- src/components/TheTimeTableSection.vue
+++ src/components/TheTimeTableSection.vue
@@ -9,7 +9,7 @@
         9:00 - 10:00
       </div>
 
-      <div class="content">
+      <div class="container">
         開場・受付
       </div>
     </div>
@@ -19,8 +19,38 @@
         10:00 - 10:10
       </div>
 
-      <div class="content">
+      <div class="container">
         オープニング
+      </div>
+    </div>
+
+    <div class="event">
+      <div class="time">
+        10:10 - 11:10
+      </div>
+
+      <div class="container">
+        <div
+          v-lazy-container="{ selector: 'img.avatar' }"
+          class="keynote"
+        >
+          <img
+            class="avatar"
+            :data-srcset="`${avatar}, ${avatar2x} 2x`"
+            :data-src="avatar2x"
+            alt=""
+          />
+
+          <div class="keynote-content">
+            <div class="title">
+              キーノート
+            </div>
+
+            <div class="speaker-name">
+              Evan You
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </BaseSection>
@@ -35,7 +65,10 @@ import BaseSection from '~/components/BaseSection.vue'
     BaseSection
   }
 })
-export default class TheTimeTableSection extends Vue {}
+export default class TheTimeTableSection extends Vue {
+  private avatar = require('~/assets/images/speakers/evan.jpg')
+  private avatar2x = require('~/assets/images/speakers/evan@2x.jpg')
+}
 </script>
 
 <style lang="scss" scoped>
@@ -47,7 +80,7 @@ export default class TheTimeTableSection extends Vue {}
   text-align: center;
 
   & + & {
-    margin-top: 20px;
+    margin-top: 4vw;
   }
 }
 
@@ -58,10 +91,33 @@ export default class TheTimeTableSection extends Vue {}
   color: $primary-text-color--invert;
 }
 
-.content {
-  margin-top: 5px;
+.container {
+  margin-top: 1vw;
   padding: 5vw;
   background-color: rgba(255, 255, 255, 0.85);
   font-size: 4vw;
+}
+
+.keynote {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.avatar {
+  width: 16vw;
+}
+
+.keynote-content {
+  margin-left: 3vw;
+  text-align: left;
+}
+
+.title {
+  font-size: 4vw;
+}
+
+.speaker-name {
+  font-size: 5.6vw;
 }
 </style>
