c48f67545e8eaba39cd4ea3563e4788380da238d
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-07 18:04:34 +0900

    new file: src/components/TheTimeTableSection.vue

---
+++ src/components/TheTimeTableSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseSection class="the-time-table-section">
+    <template slot="heading">
+      TIME TABLE
+    </template>
+  </BaseSection>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import BaseSection from '~/components/BaseSection.vue'
+
+@Component({
+  components: {
+    BaseSection
+  }
+})
+export default class TheTimeTableSection extends Vue {}
+</script>
