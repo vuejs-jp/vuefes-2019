51826c551113329e16684506606982740eaae66c
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-08 19:36:55 +0900

    viewport の width が 981px のときでも、ハーフセッションが改行されないようにフォントサイズなどを調整した（lg）

--- src/components/TheTimeTableSection.vue
+++ src/components/TheTimeTableSection.vue
@@ -10,7 +10,9 @@
       </div>
 
       <div class="event__body">
-        開場・受付
+        <div class="event__content">
+          開場・受付
+        </div>
       </div>
     </div>
 
@@ -20,7 +22,9 @@
       </div>
 
       <div class="event__body">
-        オープニング
+        <div class="event__content">
+          オープニング
+        </div>
       </div>
     </div>
 
@@ -29,24 +33,26 @@
         10:10 - 11:10
       </div>
 
-      <div
-        v-lazy-container="{ selector: 'img.keynote__avatar' }"
-        class="event__body keynote"
-      >
-        <img
-          class="keynote__avatar"
-          :data-srcset="`${keynoteAvatar}, ${keynoteAvatar2x} 2x`"
-          :data-src="keynoteAvatar2x"
-          alt=""
-        />
+      <div class="event__body">
+        <div
+          v-lazy-container="{ selector: 'img.keynote__avatar' }"
+          class="event__content keynote"
+        >
+          <img
+            class="keynote__avatar"
+            :data-srcset="`${keynoteAvatar}, ${keynoteAvatar2x} 2x`"
+            :data-src="keynoteAvatar2x"
+            alt=""
+          />
 
-        <div class="keynote__content">
-          <div class="keynote__title">
-            キーノート
-          </div>
+          <div class="keynote__content">
+            <div class="keynote__title">
+              キーノート
+            </div>
 
-          <div class="keynote__speaker-name">
-            Evan You
+            <div class="keynote__speaker-name">
+              Evan You
+            </div>
           </div>
         </div>
       </div>
@@ -57,33 +63,35 @@
         12:00 - 12:30
       </div>
 
-      <div class="event__body session">
-        <div class="session__room session__room--plaid">
-          PLAID ルーム
+      <div class="event__body">
+        <div class="event__content session">
+          <div class="session__room session__room--plaid">
+            PLAID ルーム
+          </div>
+
+          <div class="session__content">
+            ランチセッション ランチセッション ランチセッション ランチセッション ランチセッション
+          </div>
         </div>
 
-        <div class="session__content">
-          ランチセッション ランチセッション ランチセッション ランチセッション ランチセッション
-        </div>
-      </div>
+        <div class="event__content session">
+          <div class="session__room session__room--yumemi">
+            YUMEMI ルーム
+          </div>
 
-      <div class="event__body session">
-        <div class="session__room session__room--yumemi">
-          YUMEMI ルーム
-        </div>
-
-        <div class="session__content">
-          ランチセッション
-        </div>
-      </div>
-
-      <div class="event__body session">
-        <div class="session__room session__room--yesod">
-          YESOD ルーム
+          <div class="session__content">
+            ランチセッション
+          </div>
         </div>
 
-        <div class="session__content">
-          ランチセッション
+        <div class="event__content session">
+          <div class="session__room session__room--yesod">
+            YESOD ルーム
+          </div>
+
+          <div class="session__content">
+            ランチセッション
+          </div>
         </div>
       </div>
     </div>
@@ -93,49 +101,51 @@
         14:30 - 15:10
       </div>
 
-      <div class="event__body session">
-        <div class="session__room session__room--plaid">
-          PLAID ルーム
-        </div>
-
-        <div class="session__content">
-          ランチセッション ランチセッション ランチセッション ランチセッション ランチセッション
-        </div>
-      </div>
-
-      <div class="event__body session">
-        <div class="session__room session__room--yumemi">
-          YUMEMI ルーム
-        </div>
-
-        <div class="session__content">
-          ランチセッション
-        </div>
-      </div>
-
-      <div class="event__body session">
-        <div class="session__room session__room--yesod">
-          YESOD ルーム
-        </div>
-
-        <div class="session__content">
-          <div class="half-session">
-            <div class="half-session__time">
-              13:30 - 13:50
-            </div>
-
-            <div class="half-session__content">
-              ハーフセッション
-            </div>
+      <div class="event__body">
+        <div class="event__content session">
+          <div class="session__room session__room--plaid">
+            PLAID ルーム
           </div>
 
-          <div class="half-session">
-            <div class="half-session__time">
-              13:50 - 14:10
+          <div class="session__content">
+            ランチセッション ランチセッション ランチセッション ランチセッション ランチセッション
+          </div>
+        </div>
+
+        <div class="event__content session">
+          <div class="session__room session__room--yumemi">
+            YUMEMI ルーム
+          </div>
+
+          <div class="session__content">
+            ランチセッション
+          </div>
+        </div>
+
+        <div class="event__content session">
+          <div class="session__room session__room--yesod">
+            YESOD ルーム
+          </div>
+
+          <div class="session__content">
+            <div class="half-session">
+              <div class="half-session__time">
+                13:30 - 13:50
+              </div>
+
+              <div class="half-session__content">
+                ハーフセッション
+              </div>
             </div>
 
-            <div class="half-session__content">
-              ハーフセッション
+            <div class="half-session">
+              <div class="half-session__time">
+                13:50 - 14:10
+              </div>
+
+              <div class="half-session__content">
+                ハーフセッション
+              </div>
             </div>
           </div>
         </div>
@@ -166,6 +176,8 @@ export default class TheTimeTableSection extends Vue {
 <style lang="scss" scoped>
 $session__room-width--is-small: 32vw;
 $session__room-width--is-small-up: 152px;
+$session__room-width--is-medium-up: 128px;
+$event__time-width--is-medium-up: 140px;
 
 .the-time-table-section {
   background: linear-gradient(to right bottom, $asagi, $hiwamoegi);
@@ -174,11 +186,19 @@ $session__room-width--is-small-up: 152px;
 .event {
   text-align: center;
 
+  @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+    display: flex;
+  }
+
   & + & {
     margin-top: 4vw;
 
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       margin-top: 20px;
+    }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      margin-top: 5px;
     }
   }
 
@@ -191,9 +211,22 @@ $session__room-width--is-small-up: 152px;
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       font-size: 24px;
     }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      font-size: 20px;
+      flex: 0 0 $event__time-width--is-medium-up;
+    }
   }
 
   &__body {
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      display: flex;
+      margin-left: 5px;
+      flex: 1 1 auto;
+    }
+  }
+
+  &__content {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -208,6 +241,16 @@ $session__room-width--is-small-up: 152px;
       padding: 20px 36px;
       min-height: 80px;
       font-size: 20px;
+    }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      flex: 1;
+      margin-top: 0;
+      padding: 20px 16px;
+
+      & + & {
+        margin-left: 5px;
+      }
     }
   }
 }
@@ -236,6 +279,10 @@ $session__room-width--is-small-up: 152px;
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       font-size: 20px;
     }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      font-size: 18px;
+    }
   }
 
   &__speaker-name {
@@ -254,6 +301,10 @@ $session__room-width--is-small-up: 152px;
     min-height: 120px;
   }
 
+  @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+    flex-direction: column;
+  }
+
   &__room {
     width: $session__room-width--is-small;
     padding: 1vw;
@@ -265,6 +316,10 @@ $session__room-width--is-small-up: 152px;
       width: $session__room-width--is-small-up;
       padding: 4px;
       font-size: 16px;
+    }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      width: $session__room-width--is-medium-up;
     }
 
     &--plaid {
@@ -288,6 +343,14 @@ $session__room-width--is-small-up: 152px;
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       margin-left: 32px;
       width: calc(100% - #{$session__room-width--is-small-up} - 32px);
+    }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      margin-top: 16px;
+      margin-left: 0;
+      width: 100%;
+      font-size: 18px;
+      text-align: center;
     }
   }
 }
@@ -321,6 +384,10 @@ $session__room-width--is-small-up: 152px;
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       font-size: 14px;
     }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      font-size: 12px;
+    }
   }
 
   &__content {
@@ -330,6 +397,11 @@ $session__room-width--is-small-up: 152px;
     @media screen and (min-width: $layout-breakpoint--is-small-up) {
       margin-left: 20px;
       font-size: 18px;
+    }
+
+    @media screen and (min-width: $layout-breakpoint--is-medium-up) {
+      margin-left: 12px;
+      font-size: 14px;
     }
   }
 }
