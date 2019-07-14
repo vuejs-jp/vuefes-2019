efae609179c7004bce295c01a7dc73c37260d0cc
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-15 00:55:55 +0900

    FIXME: [Vue warn]: Error in render: "Error: Session Not Found" / TODO: asyncData の Context.params を指定する方法を調べる

--- test/unit/specs/pages/sessions/speakerId.spec.ts
+++ test/unit/specs/pages/sessions/speakerId.spec.ts
@@ -1,10 +1,22 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import SessionPage from '~/pages/sessions/_speakerId.vue'
+import createFullStore from '../../utils/createFullStore'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+const store = createFullStore(Vuex)
 
 describe('SessionPage', () => {
   test.skip('レンダリングできる', () => {
-    // FIXME: [Vue warn]: Error in render: "TypeError: Cannot read property '_modulesNamespaceMap' of undefined"
-    const wrapper = mount(SessionPage)
+    // FIXME: [Vue warn]: Error in render: "Error: Session Not Found"
+    // TODO: asyncData の Context.params を指定する方法を調べる
+    const wrapper = mount(SessionPage, {
+      localVue,
+      store
+    })
     expect(wrapper.find('.session-page').isVisible()).toBeTruthy()
   })
 })
