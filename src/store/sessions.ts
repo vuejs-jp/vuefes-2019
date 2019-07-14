4cdb641b89c51ed434346de24f4bdc6380fcb4b3
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-14 16:26:08 +0900

    ゲストスピーカーのセッション情報を埋めた

--- src/store/sessions.ts
+++ src/store/sessions.ts
@@ -15,7 +15,47 @@ export const state = (): State => ({
       speakerId: 'yyx990803',
       title: 'Keynote',
       paragraphs: ['Keynote'],
-      ogImage: require('~/assets/images/session-og-images/default.png')
+      ogImage: 'default.png'
+    },
+    {
+      speakerId: 'Atinux',
+      title: 'Introduction to Nuxt 3',
+      paragraphs: [
+        'This talk will introduce Nuxt 3, what are the changes and what benefits it brings for Web development and Developer Experience.'
+      ],
+      ogImage: 'Atinux.jpg'
+    },
+    {
+      speakerId: 'alexchopin',
+      title: "What's Nuxt?",
+      paragraphs: [
+        'Discover the story and philosophy of Nuxt.js and his team. Where does it come from and where do we wish to go in the future.'
+      ],
+      ogImage: 'alexchopin.jpg'
+    },
+    {
+      speakerId: 'octref',
+      title: 'Vue Tooling について語るときに僕の語ること',
+      paragraphs: [
+        'With over two million installs, today Vetur is the most popular editor tooling plugin for editing Vue single file components.',
+        "This talk will starts with an overview of Vetur's features and then dive deep into specific features such as auto completion and type checking in Vue templates.",
+        "Besides, I'll explain Vetur's role in the overall Vue ecosystem, especially in improving the TypeScript integration. Finally, I'll talk about how Vetur could offer next generation development experience from its unique position."
+      ],
+      ogImage: 'octref.jpg'
+    },
+    {
+      speakerId: 'jlooper',
+      title: 'TBD',
+      paragraphs: ['TBD'],
+      ogImage: 'jlooper.jpg'
+    },
+    {
+      speakerId: 'chrisvfritz',
+      title: 'Enterprise Vue: Patterns That Scale',
+      paragraphs: [
+        "Thinking about or currently using Vue in a very large application, potentially also with a large, diverse team? Or are you integrating Vue into a legacy app or complex architecture? Eager to learn how to avoid common mistakes, as well as battle-tested patterns to help you build an application you won't dread maintaining? As an enterprise consultant, member of the Vue team, and trained educator, Chris will share the most important lessons he's learned in his work."
+      ],
+      ogImage: 'chrisvfritz.jpg'
     }
   ]
 })
