fd7be8b9189505ec62a438d45f7b653c056c3414
ryamakuchi <r.yamakuchi@gmail.com>
2019-07-14 18:07:22 +0900

    クラス名を修正した

--- src/pages/sessions/_speakerId.vue
+++ src/pages/sessions/_speakerId.vue
@@ -4,31 +4,33 @@
       SESSION
     </h2>
 
-    <div class="session-time">
-      40min
-    </div>
+    <div class="session">
+      <div class="session-time">
+        40min
+      </div>
 
-    <h1 class="title">
-      次世代のデザインとフロントエンド開発の GUI 化
-    </h1>
+      <h1 class="session-title">
+        次世代のデザインとフロントエンド開発の GUI 化
+      </h1>
 
-    <div class="description">
-      <p>
-        STUDIO はコードを書かずに Web サイトを作れるデザインツールです（もちろん Vue.js 製！）。別の視点から見ると GUI でコードを生成するジェネレーターでもあります。
-      </p>
+      <div class="session-description">
+        <p>
+          STUDIO はコードを書かずに Web サイトを作れるデザインツールです（もちろん Vue.js 製！）。別の視点から見ると GUI でコードを生成するジェネレーターでもあります。
+        </p>
 
-      <p>
-        これまで静的な Web サイトしか作れなかった STUDIO が、今年に入ってから動的なものを作れるように開発を進めています。このとき重要になってくるのが Vue.js や React など昨今のフロントエンド開発で登場する「Component」の概念の可視化（GUI化）です。また、GUI 化によって Vue.js と React の Component の設計思想の違いが見える形で現れてきました。
-      </p>
+        <p>
+          これまで静的な Web サイトしか作れなかった STUDIO が、今年に入ってから動的なものを作れるように開発を進めています。このとき重要になってくるのが Vue.js や React など昨今のフロントエンド開発で登場する「Component」の概念の可視化（GUI化）です。また、GUI 化によって Vue.js と React の Component の設計思想の違いが見える形で現れてきました。
+        </p>
 
-      <p>
-        本セッションでは、「Component」の GUI 化を通じて見えてきた知見を中心に、実際にデモをしながら新しいデザインプロセスを提案します。
-      </p>
+        <p>
+          本セッションでは、「Component」の GUI 化を通じて見えてきた知見を中心に、実際にデモをしながら新しいデザインプロセスを提案します。
+        </p>
+      </div>
     </div>
 
     <div class="speaker">
       <img
-        class="speaker__avatar"
+        class="speaker-avatar"
         :srcset="`
           ${require('~/assets/images/speakers/keimakai1993.jpg')},
           ${require('~/assets/images/speakers/keimakai1993@2x.jpg')} 2x
@@ -37,15 +39,15 @@
         alt=""
       />
 
-      <p class="speaker__organisation">
+      <p class="speaker-organisation">
         STUDIO 株式会社
       </p>
 
-      <h2 class="speaker__name">
+      <h2 class="speaker-name">
         甲斐 啓真
       </h2>
 
-      <div class="speaker__description">
+      <div class="speaker-description">
         <p>
           STUDIO 株式会社 Founder / CPO
         </p>
@@ -55,7 +57,7 @@
         </p>
       </div>
 
-      <div class="speaker__social">
+      <div class="speaker-social">
         <a
           class="twitter"
           href="https://twitter.com/keimakai1993"
@@ -105,33 +107,40 @@ export default class SessionPage extends Vue {}
 .heading {
   font-size: 10.41vw;
   font-weight: bold;
-  line-height: 1;
-  margin-bottom: 4.4vw;
+  line-height: 1.2;
+  margin-bottom: 5vw;
 
   @media screen and (min-width: $layout-breakpoint--is-small-up) {
     font-size: 80px;
-    margin-bottom: 34px;
+    margin-bottom: 40px;
   }
 }
 
-.session-time {
-  font-size: 2vw;
-  color: $primary-text-color--invert;
-  background-color: $primary-color;
-  width: 10vw;
-  text-align: center;
-  margin-bottom: 2px;
-}
+.session {
+  &-time {
+    font-size: 2vw;
+    color: $primary-text-color--invert;
+    background-color: $primary-color;
+    width: 10vw;
+    text-align: center;
+    margin-bottom: 2px;
 
-.title {
-  font-size: 5vw;
-  font-weight: bold;
-  line-height: 1.2;
-  margin-bottom: 5vw;
-}
+    @media screen and (min-width: $layout-breakpoint--is-small-up) {
+      font-size: 12px;
+      width: 60px;
+    }
+  }
 
-.description {
-  margin-bottom: 10vw;
+  &-title {
+    font-size: 5vw;
+    font-weight: bold;
+    line-height: 1.2;
+    margin-bottom: 5vw;
+  }
+
+  &-description {
+    margin-bottom: 10vw;
+  }
 }
 
 .speaker {
@@ -146,7 +155,7 @@ export default class SessionPage extends Vue {}
     font-size: 3vw;
   }
 
-  &__avatar {
+  &-avatar {
     position: absolute;
     top: calc(-40.8vw - -5vw);
     left: -5vw;
@@ -157,18 +166,18 @@ export default class SessionPage extends Vue {}
     box-shadow: 4px 8px 16px rgba(0, 0, 0, 0.1);
   }
 
-  &__organisation {
+  &-organisation {
     margin-top: 2vw;
   }
 
-  &__name {
+  &-name {
     font-size: 6vw;
     font-weight: bold;
     line-height: 1;
     margin-bottom: 1vw;
   }
 
-  &__social {
+  &-social {
     margin-top: 2vw;
     font-size: 0;
 
