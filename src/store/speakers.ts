8f2af5bec2ba59a16f73481a375c7acaf9bd6750
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-15 03:11:46 +0900

    rootState と rootGetters を追加した

--- src/store/speakers.ts
+++ src/store/speakers.ts
@@ -13,7 +13,9 @@ type State = {
   speakers: Speaker[]
 }
 
-type Getters<S, G> = { [K in keyof G]: (state: S, getters: G) => G[K] }
+type Getters<S, G, RS = {}, RG = {}> = {
+  [K in keyof G]: (state: S, getters: G, rootState: RS, rootGetters: RG) => G[K]
+}
 
 interface IGetters {
   all: Speaker[]
