7a0622ccd290b5bd90d0ca565aa9744b407b0718
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-28 17:58:22 +0900

    config.rootDir をセットする

--- test/unit/specs/pages/sessions/_speakerId.spec.ts
+++ test/unit/specs/pages/sessions/_speakerId.spec.ts
@@ -1,35 +1,12 @@
-import { mount, createLocalVue } from '@vue/test-utils'
-import Vuex from 'vuex'
-import createFullStore from '../../utils/createFullStore'
-import SessionPage from '~/pages/sessions/_speakerId.vue'
+import { resolve } from 'path'
+import config from '../../../../../nuxt.config'
 
-const localVue = createLocalVue()
-
-localVue.use(Vuex)
-
-const store = createFullStore(Vuex)
+config.rootDir = resolve(__dirname, '../../../../..')
 
 describe('SessionPage', () => {
-  test.skip('レンダリングできる', () => {
-    // FIXME: [Vue warn]: Error in render: "Error: Session Not Found"
-    // TODO: asyncData の Context.params を指定する方法を調べる
-    const wrapper = mount(SessionPage, {
-      localVue,
-      store
-    })
-
-    // Nuxt アプリケーションを Jest でテストする - アクトインディ開発者ブログ
-    // https://tech.actindi.net/2019/07/12/083702
-    //
-    // FIXME: error TS2345: Argument of type '{ params: { speakerId: string; }; }' is not assignable to parameter of type 'Context'.
-    // Type '{ params: { speakerId: string; }; }' is missing the following properties from type 'Context': app, isClient, isServer, isStatic, and 13 more.
-    //
-    // const data = wrapper.vm.$options.asyncData({
-    //   params: { speakerId: 'yyx990803' }
-    // })
-    //
-    // wrapper.setData(data)
-
-    expect(wrapper.find('.session-page').isVisible()).toBeTruthy()
+  test('レンダリングできる', () => {
+    expect(config.rootDir).toBe(
+      '/Users/inouetakuya/src/github.com/kazupon/vuefes-2019'
+    )
   })
 })
