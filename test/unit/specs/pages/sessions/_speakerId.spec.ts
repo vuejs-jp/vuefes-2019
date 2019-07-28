b45f8daba4704c8781e77fce51c7771d0d8564fb
INOUE Takuya <inouetakuya5@gmail.com>
2019-07-28 18:31:10 +0900

    FIXME: TypeError: this._ssrNode is not a function

--- test/unit/specs/pages/sessions/_speakerId.spec.ts
+++ test/unit/specs/pages/sessions/_speakerId.spec.ts
@@ -1,12 +1,37 @@
+/**
+ * TypeError: timer.unref is not a function · Issue #1909 · facebook/jest
+ * https://github.com/facebook/jest/issues/1909#issuecomment-252981401
+ * https://jestjs.io/docs/en/configuration#testenvironment-string
+ *
+ * @jest-environment node
+ */
+
 import { resolve } from 'path'
+import { Nuxt, Builder } from 'nuxt'
 import config from '../../../../../nuxt.config'
 
-config.rootDir = resolve(__dirname, '../../../../..')
-
 describe('SessionPage', () => {
-  test('レンダリングできる', () => {
-    expect(config.rootDir).toBe(
-      '/Users/inouetakuya/src/github.com/kazupon/vuefes-2019'
+  let nuxt
+
+  beforeAll(async () => {
+    jest.setTimeout(60 * 1000)
+
+    config.dev = false
+    config.rootDir = resolve(__dirname, '../../../../..')
+    nuxt = new Nuxt(config)
+    await new Builder(nuxt).build()
+    await nuxt.listen(4000, 'localhost')
+  })
+
+  afterAll(() => {
+    nuxt.close()
+  })
+
+  test.skip('レンダリングできる', async () => {
+    // FIXME: TypeError: this._ssrNode is not a function
+    const { html } = await nuxt.renderRoute('/sessions/yyx990803/')
+    expect(html).toContain(
+      '<title data-n-head="true">Keynote（Evan You） | Vue Fes Japan 2019</title>'
     )
   })
 })
