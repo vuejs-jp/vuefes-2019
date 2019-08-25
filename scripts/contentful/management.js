7f3d9fee3747a51388ed3df8c6734f9f64854ba1
INOUE Takuya <inouetakuya5@gmail.com>
2019-08-26 00:33:46 +0900

    お試しで Speaker 情報をひとつ登録した

--- scripts/contentful/management.js
+++ scripts/contentful/management.js
@@ -10,13 +10,28 @@ const client = contentful.createClient({
   accessToken: PERSONAL_ACCESS_TOKEN
 })
 
-client.getSpace(SPACE_ID).then(space => {
-  space
-    .getEntries()
-    .then(entries => {
-      console.log(entries)
+const data = {
+  title: { 'en-US': 'Vetur クリエーター' },
+  name: { 'en-US': 'Pine Wu' },
+  twitter: { 'en-US': 'octref' },
+  github: { 'en-US': 'octref' },
+  description: {
+    'en-US':
+      'Microsoft で Visual Studio Code（VS Code）の開発に携わっています。VS Code の有名な Vue 拡張機能 Vetur の作者でもあります。Vetur という名前は、菅野よう子さんの歌『Von』に由来しています。好きなマンガは『バガボンド』で、時間があれば何度も読んでいます。'
+  }
+}
+
+client
+  .getSpace(SPACE_ID)
+  .then(space => space.getEnvironment('master'))
+  .then(environment =>
+    environment.createEntry('speaker', {
+      fields: data
     })
-    .catch(error => {
-      console.error(error)
-    })
-})
+  )
+  .then(entry => {
+    console.log(entry)
+  })
+  .catch(error => {
+    console.error(error)
+  })
