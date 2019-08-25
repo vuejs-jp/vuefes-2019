710b8fd3f93f3f8b72357f4e6de6c90d7c14c9dd
INOUE Takuya <inouetakuya5@gmail.com>
2019-08-26 01:28:58 +0900

    既存の Session 情報を Contentful に登録した

--- scripts/contentful/management.js
+++ scripts/contentful/management.js
@@ -174,28 +174,18 @@ function createSessions() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const sessions = [
     {
-      title: { 'en-US': 'Vue Tooling について語るときに僕の語ること' },
-      time: 40,
-      description: {
-        'en-US':
-          'With over two million installs, today Vetur is the most popular editor tooling plugin for editing Vue single file components.\n\n' +
-          "This talk will start with an overview of Vetur's capabilities and then dive deep into specific features such as auto completion and type checking in Vue templates.\n\n" +
-          "Besides, I'll explain Vetur's role in the overall Vue ecosystem, especially in improving the TypeScript integration. Finally, I'll talk about how Vetur could offer next generation development experience from its unique position."
-      }
-    },
-    {
       title: { 'en-US': 'TBD' },
-      time: 40,
+      time: { 'en-US': 40 },
       description: { 'en-US': 'TBD' }
     },
     {
       title: { 'en-US': 'The Present and Future about VuePress' },
-      time: 40,
+      time: { 'en-US': 40 },
       description: { 'en-US': 'TBD' }
     },
     {
       title: { 'en-US': 'Enterprise Vue: Patterns That Scale' },
-      time: 40,
+      time: { 'en-US': 40 },
       description: {
         'en-US':
           "Thinking about or currently using Vue in a very large application, potentially also with a large, diverse team? Or are you integrating Vue into a legacy app or complex architecture? Eager to learn how to avoid common mistakes, as well as battle-tested patterns to help you build an application you won't dread maintaining? As an enterprise consultant, member of the Vue team, and trained educator, Chris will share the most important lessons he's learned in his work."
@@ -203,7 +193,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': 'アンチパターンから学ぶ Atomic Design with Vue.js' },
-      time: 40,
+      time: { 'en-US': 40 },
       description: {
         'en-US':
           '誰かに教わったデザインフレームワークやプラクティスをそのまま現場に適応しようとして、うまくいかなかった経験はないでしょうか。これはフロントエンド開発に限らずアジャイル開発やインフラ設計など普遍的な問題です。\n\n' +
@@ -223,7 +213,7 @@ function createSessions() {
         'en-US':
           '4000万人が使うサービスを jQuery から Nuxt.js にまで置き換えていった話'
       },
-      time: 40,
+      time: { 'en-US': 40 },
       description: {
         'en-US':
           'Retty は、月間 4000万人のユニークユーザーを持つ実名型グルメサービスです。9年間運用されてきましたが、当時の JavaScript はフル jQuery で書かれていました。本セッションでは、ここ数年で実施したフロントエンドの改善、サービス運用と並行しながら部分的に Vue.js、Nuxt.js に移行した軌跡をお話しします。\n\n' +
@@ -233,7 +223,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': '次世代のデザインとフロントエンド開発の GUI 化' },
-      time: 40,
+      time: { 'en-US': 40 },
       description: {
         'en-US':
           'STUDIO はコードを書かずに Web サイトを作れるデザインツールです（もちろん Vue.js 製！）。別の視点から見ると GUI でコードを生成するジェネレーターでもあります。\n\n' +
@@ -243,7 +233,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': 'TypeScript CompilerAPI で挑む Vuex の型推論' },
-      time: 40,
+      time: { 'en-US': 40 },
       description: {
         'en-US':
           'Vue.js のみならず、Nuxt.js でコア機能として存在する Vuex。Vue.js アプリケーションに TypeScript を導入するにあたり、Vuex の型定義は誰しもが悩んできた課題です。vuex-module-decorators などによるアプローチもありますが、TypeScript の機能をフル活用すれば、純粋な Vuex であっても、隅々まで TypeScript に最適化することが可能です。これは、既存の Vuex コードを TypeScript 化することはもちろん、デコレーターを利用したくないシーンで役に立つ TIPS です。本セッションでは、TypeScript CompilerAPI による codegen 手法により、型推論が不可能とされていた領域に挑戦します。'
@@ -254,7 +244,7 @@ function createSessions() {
         'en-US':
           'Vue CLI、GraphQL を用いた一休.com レストラン管理画面のリニューアル'
       },
-      time: 20,
+      time: { 'en-US': 20 },
       description: {
         'en-US':
           'Vue CLI、GraphQL、Vue Router、Vuex、Bulma を用いて、一休.com レストランの管理画面リニューアルプロジェクトを高速に立ち上げ、リリースした知見を紹介します。具体的には以下のトピックについてお話しします。\n\n' +
@@ -263,7 +253,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': 'Vue.js meets Web Accessibility' },
-      time: 20,
+      time: { 'en-US': 20 },
       description: {
         'en-US':
           '「Web アクセシビリティ」とは、あらゆるユーザーが共通のコストで情報にアクセスしたりサービスを利用できるようになることです。\n\n' +
@@ -274,7 +264,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': 'Nuxt.js + TypeScript で 01 開発' },
-      time: 20,
+      time: { 'en-US': 20 },
       description: {
         'en-US':
           'TypeScript のニーズが急速に高まっていますが、実際には日本国内での知見共有や、企業における事例はまだまだ少ない状況です。Vuex で TypeScript をどう扱うかについても、未だに物議を醸しています。\n\n' +
@@ -284,7 +274,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': '型がある楽しさとつらさ' },
-      time: 20,
+      time: { 'en-US': 20 },
       description: {
         'en-US':
           '大規模サービスになると、型に守られた世界でコードを書きたいものです。実名型グルメサービス Retty でも、一部アーキテクチャの刷新時に、フロントエンドに Nuxt.js + TypeScript、サーバーとの通信に GraphQL を導入しました。\n\n' +
@@ -294,7 +284,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': '“何もない” を作る、コンポーネント指向な余白設計' },
-      time: 20,
+      time: { 'en-US': 20 },
       description: {
         'en-US':
           'Vue.js がそうであるように、限定されたスコープの CSS を使って UI コンポーネントを設計することが世の中の当たり前になってきました。それを用いて UI コンポーネントを実装する時に必ず突き当たる難題のひとつが「余白」です。本セッションでは、UI コンポーネントの開発時に検討すべき余白設計についてお話しします。\n\n' +
@@ -305,7 +295,7 @@ function createSessions() {
     },
     {
       title: { 'en-US': 'とある Nuxt.js 案件の本当にあった怖い話。' },
-      time: 20,
+      time: { 'en-US': 20 },
       description: {
         'en-US':
           'これまで 15件以上 Vue.js 案件のコンサルティングと開発に携わってきた内の 1つ、某スタートアップでの Nuxt.js 開発案件は、参加した時点で既にメンテナンス不能状態となっていました。\n\n' +
@@ -322,31 +312,22 @@ function createSessions() {
     }
   ]
 
-  const session = {
-    title: { 'en-US': 'Vue Tooling について語るときに僕の語ること' },
-    time: { 'en-US': 40 },
-    description: {
-      'en-US':
-        'With over two million installs, today Vetur is the most popular editor tooling plugin for editing Vue single file components.\n\n' +
-        "This talk will start with an overview of Vetur's capabilities and then dive deep into specific features such as auto completion and type checking in Vue templates.\n\n" +
-        "Besides, I'll explain Vetur's role in the overall Vue ecosystem, especially in improving the TypeScript integration. Finally, I'll talk about how Vetur could offer next generation development experience from its unique position."
-    }
-  }
-
-  client
-    .getSpace(SPACE_ID)
-    .then(space => space.getEnvironment('master'))
-    .then(environment =>
-      environment.createEntry('session', {
-        fields: session
+  sessions.forEach(session => {
+    client
+      .getSpace(SPACE_ID)
+      .then(space => space.getEnvironment('master'))
+      .then(environment =>
+        environment.createEntry('session', {
+          fields: session
+        })
+      )
+      .then(entry => {
+        console.log(entry)
       })
-    )
-    .then(entry => {
-      console.log(entry)
-    })
-    .catch(error => {
-      console.error(error)
-    })
+      .catch(error => {
+        console.error(error)
+      })
+  })
 }
 
 // createSpeakers()
