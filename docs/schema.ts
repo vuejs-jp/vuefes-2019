bea1a9df707426ac4ca71bc86ccf09b8e5a6c0fa
INOUE Takuya <inouetakuya5@gmail.com>
2019-08-27 09:28:57 +0900

    EventContainer はそのイベントがどの Room で行われるかについて責任を持つ

--- docs/schema.ts
+++ docs/schema.ts
@@ -21,11 +21,6 @@ type Session = {
   description: string // Markdown
   ogImage: string // Image の URL
   speakers: Speaker[] // ひとつのセッションに複数のスピーカーが入るものがある（例: Nuxt
-
-  // Room が null のときは部屋がないという意味ではなく、
-  // どこか 1つのルームに限定しないという意味。
-  // 2つ以上のルームを合体させた場合など。
-  room?: Room
 }
 
 /**
@@ -41,13 +36,14 @@ type TimeTableSection = {
 type EventContainer = {
   titleForContentful: string // Contentful での操作時にどうしても識別するタイトルが必要であるため
 
+  // Room が null のときは部屋がないという意味ではなく、
+  // どこか 1つのルームに限定しないという意味。
+  // 2つ以上のルームを合体させた場合など。
+  room?: Room
+
   // 最初の要素の sys.contentType.sys.id によって
   // Event なのか Session なのか EventContainerPart なのかを判定する
   contents: Event[] | Session[] | EventContainerPart[]
-
-  // Room は EventContainer 内で共通である
-  // contents 最初の Event、Session の Room
-  // もしくは最初の EventContainerPart の content の Room が EventContainer の Room となる
 }
 
 type EventContainerPart = {
@@ -62,11 +58,6 @@ type EventContainerPart = {
 // type Event が既に存在するため interface にした
 interface Event {
   title: string
-
-  // Room が null のときは部屋がないという意味ではなく、
-  // どこか 1つのルームに限定しないという意味。
-  // 2つ以上のルームを合体させた場合など。
-  room?: Room
 }
 
 type Room = {
