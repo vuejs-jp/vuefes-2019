c8ede59d277afa1b9b8144f3ecaebab4495f1c07
INOUE Takuya <inouetakuya5@gmail.com>
2019-08-27 16:39:30 +0900

    Access セクションのデザインを変更（md）

--- src/components/TheAccessSection.vue
+++ src/components/TheAccessSection.vue
@@ -4,46 +4,51 @@
       ACCESS
     </template>
 
-    <div class="image-container">
-      <div v-lazy-container="{ selector: 'img' }" class="image">
-        <img
-          :data-srcset="`${image}, ${image2x} 2x`"
-          :data-src="image2x"
-          alt=""
-        />
+    <div class="access-container">
+      <div class="image-container">
+        <div v-lazy-container="{ selector: 'img' }" class="image">
+          <img
+            :data-srcset="`${image}, ${image2x} 2x`"
+            :data-src="image2x"
+            alt=""
+          />
+        </div>
       </div>
-    </div>
 
-    <h3 class="title">
-      会場: TOC五反田メッセ
-    </h3>
+      <div class="access-content">
+        <h3 class="title">
+          会場: TOC五反田メッセ
+        </h3>
 
-    <div class="description">
-      <p>
-        東京都品川区西五反田6-6-19<br />
-        <a
-          href="http://messe.toc.co.jp/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          http://messe.toc.co.jp/
-        </a>
-      </p>
+        <div class="description">
+          <p>
+            東京都品川区西五反田6-6-19<br />
+            <a
+              href="http://messe.toc.co.jp/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              http://messe.toc.co.jp/
+            </a>
+          </p>
 
-      <p>
-        <span class="line">JR 山手線、都営地下鉄浅草線でお越しの方</span><br />
-        五反田駅 西口より徒歩10分
-      </p>
+          <p>
+            <span class="line">JR 山手線、都営地下鉄浅草線でお越しの方</span>
+            <br />
+            五反田駅 西口より徒歩10分
+          </p>
 
-      <p>
-        <span class="line">東急池上線でお越しの方</span><br />
-        大崎広小路駅より徒歩9分
-      </p>
+          <p>
+            <span class="line">東急池上線でお越しの方</span><br />
+            大崎広小路駅より徒歩9分
+          </p>
 
-      <p>
-        <span class="line">東急目黒線でお越しの方</span><br />
-        不動前駅より徒歩6分
-      </p>
+          <p>
+            <span class="line">東急目黒線でお越しの方</span><br />
+            不動前駅より徒歩6分
+          </p>
+        </div>
+      </div>
     </div>
 
     <BaseButton
@@ -80,10 +85,34 @@ export default class TheAccessSection extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.access-container {
+  margin-bottom: 8vw;
+
+  @media screen and (min-width: $layout-breakpoint--is-small-up) {
+    display: flex;
+    margin-bottom: 64px;
+  }
+}
+
 .image-container {
   img {
     display: block;
     width: 100%;
+  }
+
+  @media screen and (min-width: $layout-breakpoint--is-small-up) {
+    flex: 1 1 430px;
+    align-self: center;
+    order: 1;
+  }
+}
+
+.access-content {
+  @media screen and (min-width: $layout-breakpoint--is-small-up) {
+    flex: 1 1 360px;
+    min-width: 360px;
+    margin-right: 32px;
+    padding-left: 32px;
   }
 }
 
@@ -94,14 +123,12 @@ export default class TheAccessSection extends Vue {
   line-height: 1.28;
 
   @media screen and (min-width: $layout-breakpoint--is-small-up) {
-    margin-bottom: 3px;
+    margin: 0 0 4px;
     font-size: 30px;
   }
 }
 
 .description {
-  margin-bottom: 8vw;
-
   p {
     font-size: 3.5vw;
 
@@ -116,6 +143,10 @@ export default class TheAccessSection extends Vue {
 
   p + p {
     margin-top: 5vw;
+
+    @media screen and (min-width: $layout-breakpoint--is-small-up) {
+      margin-top: 32px;
+    }
   }
 }
 
