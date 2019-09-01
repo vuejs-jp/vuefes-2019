f2293ab274dc53b8afdc0a7535bcc7e5d4add2bd
numa <aytdm251@gmail.com>
2019-09-02 01:35:45 +0900

    doc: チケットセクションの文言を変更 (#169)
    * doc: チケットセクションの文言を変更
    
    * doc: レビュー内容を反映

--- src/components/TheTicketSection.vue
+++ src/components/TheTicketSection.vue
@@ -7,18 +7,23 @@
     <div class="description">
       <!-- prettier-ignore -->
       <p>
-        Vue Fes Japan 2019 のチケットが発売中です。チケットはアフターパーティーあり/なしの 2種類で、全席ランチ付きです。チケットの購入は「チケットを購入する」ボタンからご購入ください。Apple Pay で決済をご希望の方は、<a href="#" class="link" @click.prevent="openUniverse">こちらのリンク</a>から直接ご購入ください。
+        Vue Fes Japan 2019 のチケットが発売中です。チケットはアフターパーティーあり/なしの 2種類で、全席ランチ付きです。チケットの購入は「チケットを購入する」ボタンからご購入ください。
       </p>
 
       <!-- prettier-ignore -->
       <p>
-        購入時に届くメールに QR コードが記載されていますので、そちらをチケットとして当日ご持参ください。
+        販売媒体として <a href="https://www.universe.com/" target="_blank" rel="noopener" class="link">Universe</a> を利用します。購入時に届くメールに QR コードが記載されていますので、そちらをチケットとして当日ご持参ください。
+      </p>
+
+      <!-- prettier-ignore -->
+      <p>
+        Apple Pay で決済をご希望の方は、<a href="#" class="link" @click.prevent="openUniverse">こちらのリンク</a>から直接ご購入ください。また、Universe の決済方法でチケットを購入できない方向けに別途購入窓口を開設しています。詳細は <a href="https://note.mu/ryamakuchi/n/n7a07b20d811d" target="_blank" rel="noopener noreferrer" class="link">note</a> をご覧ください。
       </p>
 
       <!-- prettier-ignore -->
       <p>
         <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
-        チケットに関するお問い合わせは、<nuxt-link class="link" to="/contact/">お問い合わせページ</nuxt-link>からお願いします。
+        チケットに関するお問い合わせは、Vue Fes Japan 2019の<nuxt-link class="link" to="/contact/">お問い合わせフォーム</nuxt-link>よりお問い合わせください。
       </p>
     </div>
 
@@ -135,6 +140,26 @@
         </h4>
         <p>
           全セッション終了後、会場内にて立食形式のお食事、お飲み物（アルコールあり）を召し上がりながらご歓談いただけます。お1人で参加される方でもお楽しみいただけるような工夫を行う予定です。お楽しみに！
+        </p>
+      </div>
+
+      <div class="faq-content">
+        <h4>
+          チケットはいつまで買えますか？
+        </h4>
+        <!-- prettier-ignore -->
+        <p>
+          2019/9/17（火）23:59まで購入できます。
+        </p>
+      </div>
+
+      <div class="faq-content">
+        <h4>
+          チケットを購入しましたが、メールが届きません
+        </h4>
+        <!-- prettier-ignore -->
+        <p>
+          迷惑メールとして扱われている可能性があります。迷惑メールに振り分けられていないかご確認ください。それでも届いていない場合、迷惑メールの対策のため受信できていない可能性があります。「universe.com」を受信できるように設定のうえ、Vue Fes Japan 2019の<nuxt-link class="link" to="/contact/">お問い合わせフォーム</nuxt-link>よりお問い合わせください。
         </p>
       </div>
 
