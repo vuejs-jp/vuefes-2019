90900c753b09e812be56f16a6adc67012977014b
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-01 23:22:49 +0900

    Home と同じようにスポンサー一覧を表示した

--- src/pages/sponsors.vue
+++ src/pages/sponsors.vue
@@ -5,9 +5,41 @@
     </template>
     スポンサー詳細ページ
 
-    <pre>
-      {{ sponsors }}
-    </pre>
+    <ul v-for="sponsorPlan in sponsorPlans" :key="sponsorPlan.plan">
+      <li
+        v-if="sponsorsByPlan(sponsorPlan.plan).length > 0"
+        class="sponsor-group"
+        :class="sponsorPlan.plan"
+      >
+        <h3 class="sponsor-plan">
+          {{ sponsorPlan.name }}
+        </h3>
+
+        <ul
+          v-for="sponsor in sortSponsors(sponsorsByPlan(sponsorPlan.plan))"
+          :key="sponsor.sys.id"
+          class="sponsor"
+        >
+          <li>
+            <a :href="sponsor.fields.url" target="_blank" rel="noopener">
+              <div v-lazy-container="{ selector: 'img' }">
+                <img
+                  class="sponsor-image"
+                  :data-src="sponsor.fields.banner.fields.file.url"
+                  :alt="sponsor.fields.name"
+                />
+              </div>
+            </a>
+          </li>
+
+          <li>{{ sponsor.fields.name }}</li>
+
+          <li>{{ sponsor.fields.url }}</li>
+
+          <li>{{ sponsor.fields.pr }}</li>
+        </ul>
+      </li>
+    </ul>
   </BaseMain>
 </template>
 <script lang="ts">
@@ -33,6 +65,35 @@ import BaseButton from '~/components/BaseButton.vue'
   }
 })
 export default class SponsorsPage extends Vue {
+  sponsorPlans: { plan: string; name: string }[] = [
+    { plan: 'platinum', name: 'PLATINUM' },
+    { plan: 'gold', name: 'GOLD' },
+    { plan: 'silver', name: 'SILVER' },
+    { plan: 'bronze', name: 'BRONZE' },
+    { plan: 'special', name: 'SPECIAL' },
+    { plan: 'room', name: 'ROOM' },
+    { plan: 'translation', name: '同時通訳' },
+    { plan: 'commercial', name: '幕間 CM' },
+    { plan: 'toast', name: 'カンパイ' },
+    { plan: 'lunch', name: 'LUNCH' },
+    { plan: 'refreshment', name: 'REFRESHMENT' },
+    { plan: 'video', name: 'VIDEO' },
+    { plan: 'media', name: 'MEDIA' }
+  ]
+
+  sortSponsors(sponsors): Entry<any>[] {
+    return sponsors.sort((a, b) => {
+      if (a.fields.appliedAt < b.fields.appliedAt) return -1
+      if (a.fields.appliedAt > b.fields.appliedAt) return 1
+
+      return 0
+    })
+  }
+
+  sponsorsByPlan(plan): Entry<any>[] {
+    return this.$data.sponsors.filter(sponsor => sponsor.fields.plan === plan)
+  }
+
   private head() {
     const url = `https://vuefes.jp/2019/sponsors`
     const title = 'スポンサー一覧 | Vue Fes Japan 2019'
