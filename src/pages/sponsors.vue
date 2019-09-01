2a8762554079a7c2c2665c89c799acd07c307887
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-01 18:38:37 +0900

    add pages/sponsors.vue

---
+++ src/pages/sponsors.vue
@@ -0,0 +1,49 @@
+<template>
+  <BaseMain class="sponsors-page">
+    <template slot="heading">
+      SPONSORS
+    </template>
+    スポンサー詳細ページ
+  </BaseMain>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import BaseMain from '~/components/BaseMain.vue'
+import BaseButton from '~/components/BaseButton.vue'
+
+@Component({
+  components: {
+    BaseMain,
+    BaseButton
+  },
+  asyncData({ route }) {
+    return {
+      path: route.path
+    }
+  }
+})
+export default class SponsorsPage extends Vue {
+  path!: string
+
+  private head() {
+    const url = `https://vuefes.jp/2019${this.path}`
+    const title = 'スポンサー一覧 | Vue Fes Japan 2019'
+    const description = 'Vue Fes Japan 2019 のスポンサー情報です。'
+    return {
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'og:url', name: 'og:url', content: url },
+        { hid: 'og:title', name: 'og:title', content: title },
+        { hid: 'og:description', name: 'og:description', content: description },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: description
+        }
+      ]
+    }
+  }
+}
+</script>
