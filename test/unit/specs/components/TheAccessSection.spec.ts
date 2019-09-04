cbb3ee611ad3e57a3b6cb31393185269e3e7f97b
INOUE Takuya <inouetakuya5@gmail.com>
2019-09-04 23:07:28 +0900

    Jest 実行中に出る warning を修正した

--- test/unit/specs/components/TheAccessSection.spec.ts
+++ test/unit/specs/components/TheAccessSection.spec.ts
@@ -1,9 +1,14 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import VueLazyLoad from 'vue-lazyload'
 import TheAccessSection from '~/components/TheAccessSection.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(VueLazyLoad)
 
 describe('TheAccessSection', () => {
   test('レンダリングできる', () => {
-    const wrapper = mount(TheAccessSection)
+    const wrapper = mount(TheAccessSection, { localVue })
     expect(wrapper.find('.the-access-section').isVisible()).toBeTruthy()
   })
 })
--- test/unit/specs/components/TheHeaderLogo.spec.ts
+++ test/unit/specs/components/TheHeaderLogo.spec.ts
@@ -1,9 +1,15 @@
-import { mount, RouterLinkStub } from '@vue/test-utils'
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import VueLazyLoad from 'vue-lazyload'
 import TheHeaderLogo from '~/components/TheHeaderLogo.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(VueLazyLoad)
 
 describe('TheHeaderLogo', () => {
   test('トップページへのリンクをレンダリングできる', () => {
     const wrapper = mount(TheHeaderLogo, {
+      localVue,
       stubs: {
         NuxtLink: RouterLinkStub
       }
