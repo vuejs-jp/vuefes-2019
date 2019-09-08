5fdad4d571fe29d37ff7e7d1f22c06bd6eefd66d
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-09 04:42:10 +0900

    SponsorsPage にテストケースを追加した

--- test/unit/specs/pages/sponsors.spec.ts
+++ test/unit/specs/pages/sponsors.spec.ts
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import { mount, RouterLinkStub, Wrapper } from '@vue/test-utils'
+import { mount, RouterLinkStub } from '@vue/test-utils'
 import sponsorList from '../../__mocks__/sponsorListMock'
 import SponsorsPage from '~/pages/sponsors.vue'
 
 describe('SponsorsPage', () => {
-  let wrapper: Wrapper<Vue>
+  let wrapper
+    /* eslint-disable no-unused-expressions */
+  ;(<any>window).IntersectionObserver = class IntersectionObserver {
+    constructor() {
+      null
+    }
+    observe() {
+      return null
+    }
+  }
+  /* eslint-enable no-unused-expressions */
 
   beforeEach(() => {
     wrapper = mount(SponsorsPage, {
@@ -21,5 +30,27 @@ describe('SponsorsPage', () => {
 
   test('レンダリングできる', () => {
     expect(wrapper.find('.sponsors-page').isVisible()).toBeTruthy()
+  })
+
+  describe('.sponsor-group', () => {
+    test('スポンサープランがクラス名に付与される', () => {
+      expect(wrapper.find('.sponsor-group').classes()).toEqual([
+        'sponsor-group',
+        'platinum'
+      ])
+    })
+
+    test('スポンサープランが表示される', () => {
+      expect(wrapper.find('.sponsor-plan').text()).toBe('PLATINUM')
+    })
+  })
+
+  describe('sponsorsByPlan', () => {
+    test('スポンサープランによってフィルタリングできる', () => {
+      const sponsors = wrapper.vm.sponsorsByPlan('silver')
+
+      expect(sponsors.length).toBe(2)
+      expect(sponsors[0].fields.plan).toBe('silver')
+    })
   })
 })
