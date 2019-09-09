08c51acae336dae729d1f7c8bcd361d9a71bbb6e
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-10 02:16:06 +0900

    スムーズスクロールを window.scroll に切り替える

--- src/plugins/sponsor-anchor.ts
+++ src/plugins/sponsor-anchor.ts
@@ -1,11 +1,18 @@
 export default async ({ app }) => {
   await app.router.afterEach(to => {
     if (to.hash && to.name === 'sponsors') {
-      const element = document.querySelector(to.hash)
+      const element = document.querySelector(to.hash) // 飛び先を取得
+
       if (element) {
-        element.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
+        // 現在の位置から飛ぶ要素の位置までの距離を取得
+        const rectTop = element.getBoundingClientRect().top
+        // ページ上部から現在のスクロールの位置までの距離を取得
+        const offsetTop = window.pageYOffset
+        // スクロールする位置（ページ上部からの絶対値）
+        const top = rectTop + offsetTop
+        window.scroll({
+          top,
+          behavior: 'smooth'
         })
       }
     }
