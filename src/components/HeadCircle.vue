21cbd1f8b516c59231fb20ff431437e837a5db5f
INOUE Takuya <inouetakuya5@gmail.com>
2019-09-16 19:42:32 +0900

    refactor: 使われていないコードの削除や @types/gsap の追加

--- src/components/HeadCircle.vue
+++ src/components/HeadCircle.vue
@@ -7,16 +7,18 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import { TweenMax, Power2 } from 'gsap'
-import { Parts, partsCreateTime, partsLeaveTime } from './TheHeadSection.vue'
+import {
+  Parts,
+  partsCreateTime,
+  partsLeaveTime
+} from '~/components/TheHeadSection.vue'
 
 @Component
 export default class HeadCircle extends Vue {
   @Prop()
   readonly item!: Parts
 
-  t = 0
-
-  keyFrame = [0, 60]
+  private keyFrame = [0, 60]
 
   get transform() {
     return `translate(${this.item.x}, ${this.item.y}) rotate(${this.item.rotate})`
