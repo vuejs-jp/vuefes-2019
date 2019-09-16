dee43205a32516aa57956c6317789f20fbc37ff4
INOUE Takuya <inouetakuya5@gmail.com>
2019-09-16 19:12:09 +0900

    nuxtServerInit で EventContainer と EventContainerPart のデータを取得するようにした

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
 export const actions = {
   async nuxtServerInit({ dispatch }) {
+    // TODO: Contentful へ並列でリクエストを送るようにする
     await dispatch('timeTableSections/fetchTimeTableSections')
+    await dispatch('eventContainers/fetchEventContainers')
+    await dispatch('eventContainerParts/fetchEventContainerParts')
   }
 }
