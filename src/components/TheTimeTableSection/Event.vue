7cee13f97545a31a978af7e374b524b4f3e34d12
INOUE Takuya <inouetakuya5@gmail.com>
2019-09-17 00:39:35 +0900

    new file: src/components/TheTimeTableSection/Event.vue

---
+++ src/components/TheTimeTableSection/Event.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="event">
+    {{ event.fields.title }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import EventType from '~/types/event'
+
+@Component
+export default class Event extends Vue {
+  @Prop()
+  readonly event!: EventType
+}
+</script>
---
+++ test/unit/specs/components/TheTimeTableSection/Event.spec.ts
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import events from '../../../__mocks__/events'
+import Event from '~/components/TheTimeTableSection/Event.vue'
+
+describe('Event', () => {
+  test('event.fields.title を表示できる', () => {
+    const wrapper = mount(Event, {
+      propsData: {
+        event: events[0]
+      }
+    })
+    expect(wrapper.find('.event').text()).toBe(events[0].fields.title)
+  })
+})
