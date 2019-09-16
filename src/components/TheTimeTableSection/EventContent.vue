f94a2106e892e863a7e613f9115967367fb16a73
INOUE Takuya <inouetakuya5@gmail.com>
2019-09-17 01:36:57 +0900

    new file: src/components/TheTimeTableSection/EventContent.vue

---
+++ src/components/TheTimeTableSection/EventContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="event-content">
+    <template v-if="eventContent.sys.contentType.sys.id === 'session'">
+      <Session :session="eventContent" />
+    </template>
+
+    <template v-else-if="eventContent.sys.contentType.sys.id === 'event'">
+      <Event :event="eventContent" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import SessionType from '~/types/session'
+import EventType from '~/types/event'
+import Session from '~/components/TheTimeTableSection/Session.vue'
+import Event from '~/components/TheTimeTableSection/Event.vue'
+
+@Component({
+  components: {
+    Session,
+    Event
+  }
+})
+export default class EventContent extends Vue {
+  @Prop()
+  readonly eventContent!: SessionType | EventType
+}
+</script>
