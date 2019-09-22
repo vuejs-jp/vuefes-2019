b02c7afdc6e9ba1f257fe0cc88406603c4a1aa36
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-23 00:05:45 +0900

    Vuex Store sponsors のテストを追加

---
+++ test/unit/specs/store/sponsors.ts
@@ -0,0 +1,58 @@
+import sponsorList from '../../../fixtures/contentful/sponsors'
+import * as contentful from '~/plugins/contentful'
+import {
+  state as initialState,
+  getters,
+  mutations,
+  actions
+} from '~/store/sponsors'
+
+describe('sponsors module', () => {
+  let state
+
+  beforeEach(() => {
+    state = initialState()
+  })
+
+  describe('getters', () => {
+    describe('sponsorsByPlan', () => {
+      beforeEach(() => {
+        state.sponsors = sponsorList
+      })
+
+      test('plan と一致するスポンサー情報のみ取得できる', () => {
+        expect(
+          getters.sponsorsByPlan(state.sponsors)(state.sponsorPlans[0].plan)
+        ).toEqual(sponsorList[2])
+      })
+    })
+  })
+
+  describe('mutations', () => {
+    describe('setSponsors', () => {
+      test('SponsorList をセットできる', () => {
+        mutations.setSponsors(state, sponsorList)
+        expect(state.sponsors).toEqual(sponsorList)
+      })
+    })
+  })
+
+  describe('actions', () => {
+    describe('fetchSponsors', () => {
+      beforeEach(() => {
+        // @ts-ignore error TS2540: Cannot assign to 'getSponsors' because it is a read-only property.
+        contentful.getSponsors = jest.fn().mockResolvedValue(sponsorList)
+      })
+
+      test('sponsorList を取得してセットする', async () => {
+        const commit = jest.fn()
+
+        // @ts-ignore error TS2345: Argument of type '{ commit: Mock<any, any>; }' is not assignable to parameter of type 'Context<State, Actions, Getters, Mutations, {}, {}>'.
+        // Type '{ commit: Mock<any, any>; }' is missing the following properties from type 'Context<State, Actions, Getters, Mutations, {}, {}>': dispatch, state, getters, rootState, rootGettersts
+        await actions.fetchSponsors({ commit })
+
+        expect(commit).toHaveBeenCalledWith('setSponsors', sponsorList)
+      })
+    })
+  })
+})
