118f777d54ae0965dce01732d2e9b45e114b3214
yinm <yinm.cminor9@gmail.com>
2019-09-22 13:18:23 +0900

    ハンバーガーメニューで開閉できるようにする

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,6 +3,41 @@
     <nuxt-link to="/">
       <img src="~/assets/images/logo-vuefes.svg" alt="Vue Fes Japan" />
     </nuxt-link>
+    <div class="navigation-container">
+      <div class="togglable-menu">
+        <div @click="openMenu">
+          <span class="hamburger hamburger-top" />
+          <span class="hamburger hamburger-middle" />
+          <span class="hamburger hamburger-bottom" />
+        </div>
+        <nav v-show="isOpen">
+          <ul>
+            <!-- TODO: 各要素のページ内リンクをつける -->
+            <li>
+              <a href="#">TICKET</a>
+            </li>
+            <li>
+              <a href="#">TIME TABLE</a>
+            </li>
+            <li>
+              <a href="#">SPEAKERS</a>
+            </li>
+            <li>
+              <a href="#">EVENTS</a>
+            </li>
+            <li>
+              <a href="#">ACCESS</a>
+            </li>
+            <li>
+              <a href="#">SPONSERS</a>
+            </li>
+            <li>
+              <a href="#">TEAM</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +45,13 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
-export default class TheHeader extends Vue {}
+export default class TheHeader extends Vue {
+  isOpen = false
+
+  openMenu() {
+    this.isOpen = !this.isOpen
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -31,6 +72,28 @@ export default class TheHeader extends Vue {}
     img {
       height: 40px;
     }
+  }
+}
+
+.hamburger {
+  position: absolute;
+  display: inline-block;
+  content: '';
+  height: 4px;
+  width: 42px;
+  background: #34495e;
+  cursor: pointer;
+
+  &-top {
+    top: 0;
+  }
+
+  &-middle {
+    top: 15px;
+  }
+
+  &-bottom {
+    top: 30px;
   }
 }
 </style>
