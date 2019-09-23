75f20eba30b6714c1e4a976d36d7b5be0a2de0a1
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-23 09:24:31 +0900

    sponsors.vue のテストを追加する

---
+++ test/unit/specs/pages/sponsors.spec.ts
@@ -0,0 +1,56 @@
+import { mount, RouterLinkStub, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import sponsorList from '../../../fixtures/contentful/sponsors'
+import createFullStore from '../utils/createFullStore'
+import SponsorsPage from '~/pages/sponsors.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+const store = createFullStore(Vuex)
+
+describe('SponsorsPage', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(SponsorsPage, {
+      localVue,
+      store,
+      stubs: {
+        NuxtLink: RouterLinkStub
+      }
+    })
+  })
+
+  test('レンダリングできる', () => {
+    expect(wrapper.find('.sponsors-page').isVisible()).toBeTruthy()
+  })
+
+  describe('.sponsor-group-list', () => {
+    test('スポンサープランがクラス名に付与される', () => {
+      expect(wrapper.find('.sponsor-group').classes()).toEqual([
+        'sponsor-group',
+        'platinum'
+      ])
+    })
+
+    test('スポンサープランの詳細が表示される', () => {
+      expect(wrapper.find('.sponsor-plan').text()).toBe('PLATINUM')
+      expect(wrapper.find('.sponsor').attributes().id).toBe(
+        `sponsor_${sponsorList[2].sys.id}`
+      )
+      expect(wrapper.find('.sponsor > li > a').attributes().href).toBe(
+        sponsorList[2].fields.url
+      )
+      expect(wrapper.find('.sponsor-image').isVisible()).toBeTruthy()
+      expect(wrapper.find('.sponsor-name').text()).toBe(
+        sponsorList[2].fields.name
+      )
+      expect(wrapper.find('.sponsor-url').text()).toBe(
+        sponsorList[2].fields.url
+      )
+      expect(wrapper.find('.sponsor-pr').text()).toBe(sponsorList[2].fields.pr)
+    })
+  })
+})
