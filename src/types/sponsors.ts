5d0beec84be15325097141efd2c2b8a11067bf8c
ryamakuchi <r.yamakuchi@gmail.com>
2019-09-24 01:09:56 +0900

    banner の定義を Asset に変更

--- src/types/sponsors.ts
+++ src/types/sponsors.ts
@@ -1,4 +1,4 @@
-import { EntrySys, EntryLink } from '~/types/contentful'
+import { EntrySys, Asset } from '~/types/contentful'
 
 export default interface SponsorList {
   sys: EntrySys
@@ -6,37 +6,7 @@ export default interface SponsorList {
     plan: string
     name: string
     url: string
-    banner: {
-      sys: {
-        space: {
-          sys: EntryLink
-        }
-        id: string
-        type: string
-        createdAt: string
-        updatedAt: string
-        environment: {
-          sys: EntryLink
-        }
-        revision: number
-        locale: string
-      }
-      fields: {
-        title: string
-        file: {
-          url: string
-          details: {
-            size: number
-            image: {
-              width: number
-              height: number
-            }
-          }
-          fileName: string
-          contentType: string
-        }
-      }
-    }
+    banner: Asset
     pr: string
     appliedAt: string
   }
