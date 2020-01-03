a3767b77aa88ecb21186fb077833419752d5af1c
INOUE Takuya <inouetakuya5@gmail.com>
2020-01-03 22:37:02 +0900

    アセット情報取得のデバッグ

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home-page">
     <TheHeadSection />
+
+    {{ asset }}
+
     <TheSpeakerListSection />
     <TheTicketSection />
     <TheAccessSection />
@@ -21,6 +24,7 @@ import TheTicketSection from '~/components/TheTicketSection.vue'
 import TheTimeTableSection from '~/components/TheTimeTableSection.vue'
 import TheSponsorListSection from '~/components/TheSponsorListSection.vue'
 import TheStaffListSection from '~/components/TheStaffListSection.vue'
+import { getAsset } from '~/plugins/contentful'
 
 @Component({
   components: {
@@ -32,6 +36,11 @@ import TheStaffListSection from '~/components/TheStaffListSection.vue'
     TheTimeTableSection,
     TheSponsorListSection,
     TheStaffListSection
+  },
+  async asyncData() {
+    return {
+      asset: await getAsset('1Bqb7VVTxdnL3zZIh8FNJD')
+    }
   }
 })
 export default class HomePage extends Vue {}
