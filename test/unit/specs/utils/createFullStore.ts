e29563bf3b91fa5e52a84beda2e27410f28cb1f9
INOUE Takuya <inouetakuya5@gmail.com>
2020-01-05 11:40:40 +0900

    nuxtServerInit にdispatch('sessions/initialize') を追加した

--- test/unit/specs/utils/createFullStore.ts
+++ test/unit/specs/utils/createFullStore.ts
@@ -1,5 +1,6 @@
 import cloneDeep from 'lodash.clonedeep'
 import speakersData from 'test/fixtures/contentful/speakers'
+import sessionsData from 'test/fixtures/contentful/sessions'
 import timeTableSectionsData from 'test/fixtures/contentful/timeTableSections'
 import eventContainersData from 'test/fixtures/contentful/eventContainers'
 import eventContainerPartsData from 'test/fixtures/contentful/eventContainerParts'
@@ -8,6 +9,7 @@ import assetData from 'test/fixtures/contentful/asset'
 import * as localSessions from '~/store/localSessions'
 import * as localSpeakers from '~/store/localSpeakers'
 import * as speakers from '~/store/speakers'
+import * as sessions from '~/store/sessions'
 import * as timeTableSections from '~/store/timeTableSections'
 import * as eventContainers from '~/store/eventContainers'
 import * as eventContainerParts from '~/store/eventContainerParts'
@@ -17,6 +19,11 @@ const speakersDataWithAssets = cloneDeep(speakersData).map(speaker => {
   speaker.fields.avatar = assetData
   speaker.fields.avatar2x = assetData
   return speaker
+})
+
+const sessionsDataWithAssets = cloneDeep(sessionsData).map(session => {
+  session.fields.ogImage = assetData
+  return session
 })
 
 export default Vuex => {
@@ -33,6 +40,10 @@ export default Vuex => {
       speakers: {
         namespaced: true,
         ...speakers
+      },
+      sessions: {
+        namespaced: true,
+        ...sessions
       },
       timeTableSections: {
         namespaced: true,
@@ -55,6 +66,7 @@ export default Vuex => {
 
   // nuxtServerInit と同様のことを行う
   store.commit('speakers/setSpeakers', speakersDataWithAssets)
+  store.commit('sessions/setSessions', sessionsDataWithAssets)
   store.commit('timeTableSections/setTimeTableSections', timeTableSectionsData)
   store.commit('eventContainers/setEventContainers', eventContainersData)
   store.commit(
